95e33269bc2f31abb80cc6e33ea396686d6166a6
doitduri <kdle_@naver.com>
2023-02-18 18:20:04 +0900

    Font 정의 및 추가

--- DIDA/DIDA/Common/Fonts.swift
+++ DIDA/DIDA/Common/Fonts.swift
@@ -5,4 +5,22 @@
 //  Created by 김두리 on 2023/02/18.
 //
 
-import Foundation
+import UIKit
+
+final class Fonts {
+    static let bold_24 = UIFont.init(name: "Pretendard-Bold", size: 24)
+    static let bold_32 = UIFont.init(name: "Pretendard-Bold", size: 32)
+    static let bold_20 = UIFont.init(name: "Pretendard-Bold", size: 20)
+    static let bold_18 = UIFont.init(name: "Pretendard-Bold", size: 18)
+    static let bold_16 = UIFont.init(name: "Pretendard-Bold", size: 16)
+    static let bold_15 = UIFont.init(name: "Pretendard-Bold", size: 15)
+    static let bold_14 = UIFont.init(name: "Pretendard-Bold", size: 14)
+    static let extra_bold_32 = UIFont.init(name: "Pretendard-ExtraBold", size: 32)
+    
+    static let semi_bold_12 = UIFont.init(name: "Pretendard-SemiBold", size: 12)
+    static let regular_18 = UIFont.init(name: "Pretendard-Regular", size: 18)
+    static let regular_16 = UIFont.init(name: "Pretendard-Regular", size: 16)
+    static let regular_15 = UIFont.init(name: "Pretendard-Regular", size: 15)
+    static let regular_14 = UIFont.init(name: "Pretendard-Regular", size: 14)
+    static let regular_12 = UIFont.init(name: "Pretendard-Regular", size: 12)
+}
