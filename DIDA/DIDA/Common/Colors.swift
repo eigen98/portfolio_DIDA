2b4b483b6adaec94fdb70a5b0707c7f0e37b378c
doitduri <kdle_@naver.com>
2023-02-18 18:02:51 +0900

    UIColor extension 추가
    - color hex to UIColor

--- DIDA/DIDA/Common/Colors.swift
+++ DIDA/DIDA/Common/Colors.swift
@@ -8,40 +8,20 @@
 import UIKit
 
 final class Colors {
-    static let background = UIColor.init() //
-    static let text_white = UIColor.init()
-    // #ffffff
-    static let text_notice = UIColor.init()
-    // #E8625B
-    
-    static let brand_lemon = UIColor.init()
-    // #FBF89B
-    
-    static let brand_light_lemon = UIColor.init()
-    // #FDFBC4
-    
-    static let brand_ivory = UIColor.init()
-    // #FFFFE8
-    
-    static let brand_lemon_100 = UIColor.init()
-    // #FFFFE8
-    
-    static let brand_lemon_200 = UIColor.init()
-    // #FDFBC4
-    static let brand_lemon_300 = UIColor.init()
-    // #FBF89B
-    static let brand_lemon_400 = UIColor.init()
-    // #F8F470
-    static let brand_lemon_500 = UIColor.init()
-    // #FFF957
-    static let brand_lemon_600 = UIColor.init()
-    // #FCF532
-    static let brand_lemon_700 = UIColor.init()
-    // #FFE42E
-    static let brand_lemon_800 = UIColor.init()
-    // #FFCC22
-    static let brand_lemon_900 = UIColor.init()
-    // #FFB314
-    static let brand_lemon_1000 = UIColor.init()
-    // #FD8900
+    static let background = UIColor.init(hex: "#000000")
+    static let text_white = UIColor.init(hex: "#ffffff")
+    static let text_notice = UIColor.init(hex: "#E8625B")
+    static let brand_lemon = UIColor.init(hex: "#FBF89B")
+    static let brand_light_lemon = UIColor.init(hex: "#FDFBC4")
+    static let brand_ivory = UIColor.init(hex: "#FFFFE8")
+    static let brand_lemon_100 = UIColor.init(hex: "#FFFFE8")
+    static let brand_lemon_200 = UIColor.init(hex: "#FDFBC4")
+    static let brand_lemon_300 = UIColor.init(hex: "#FBF89B")
+    static let brand_lemon_400 = UIColor.init(hex: "#F8F470")
+    static let brand_lemon_500 = UIColor.init(hex: "#FFF957")
+    static let brand_lemon_600 = UIColor.init(hex: "#FCF532")
+    static let brand_lemon_700 = UIColor.init(hex: "#FFE42E")
+    static let brand_lemon_800 = UIColor.init(hex: "#FFCC22")
+    static let brand_lemon_900 = UIColor.init(hex: "#FFB314")
+    static let brand_lemon_1000 = UIColor.init(hex: "#FD8900")
 }
