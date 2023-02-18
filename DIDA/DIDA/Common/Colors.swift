c74a7f1b3c4bfa6387b37595e3cbd60fad06d6f6
doitduri <kdle_@naver.com>
2023-02-18 17:57:06 +0900

    color 에셋 정의

---
+++ DIDA/DIDA/Common/Colors.swift
@@ -0,0 +1,47 @@
+//
+//  Colors.swift
+//  DIDA
+//
+//  Created by 김두리 on 2023/02/18.
+//
+
+import UIKit
+
+final class Colors {
+    static let background = UIColor.init() //
+    static let text_white = UIColor.init()
+    // #ffffff
+    static let text_notice = UIColor.init()
+    // #E8625B
+    
+    static let brand_lemon = UIColor.init()
+    // #FBF89B
+    
+    static let brand_light_lemon = UIColor.init()
+    // #FDFBC4
+    
+    static let brand_ivory = UIColor.init()
+    // #FFFFE8
+    
+    static let brand_lemon_100 = UIColor.init()
+    // #FFFFE8
+    
+    static let brand_lemon_200 = UIColor.init()
+    // #FDFBC4
+    static let brand_lemon_300 = UIColor.init()
+    // #FBF89B
+    static let brand_lemon_400 = UIColor.init()
+    // #F8F470
+    static let brand_lemon_500 = UIColor.init()
+    // #FFF957
+    static let brand_lemon_600 = UIColor.init()
+    // #FCF532
+    static let brand_lemon_700 = UIColor.init()
+    // #FFE42E
+    static let brand_lemon_800 = UIColor.init()
+    // #FFCC22
+    static let brand_lemon_900 = UIColor.init()
+    // #FFB314
+    static let brand_lemon_1000 = UIColor.init()
+    // #FD8900
+}
---
+++ DIDA/DIDA/Common/Fonts.swift
@@ -0,0 +1,8 @@
+//
+//  Fonts.swift
+//  DIDA
+//
+//  Created by 김두리 on 2023/02/18.
+//
+
+import Foundation
