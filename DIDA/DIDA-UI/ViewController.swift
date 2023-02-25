d98c97450c5165ca7784e679df0dfd6abacb2f0d
doitduri <kdle_@naver.com>
2023-02-25 23:36:27 +0900

    DIDA-UI target 추가

---
+++ DIDA/DIDA-UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DIDA-UI
+//
+//  Created by 김두리 on 2023/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
