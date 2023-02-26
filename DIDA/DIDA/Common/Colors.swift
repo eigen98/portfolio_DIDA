c83cacd8809937304a3202f0625f0bb5357dbb5e
doitduri <kdle_@naver.com>
2023-02-27 00:24:38 +0900

    Colors, Fonts 추가
    - text_notice_red / green
    - medium_14

--- DIDA/DIDA/Common/Colors.swift
+++ DIDA/DIDA/Common/Colors.swift
@@ -10,7 +10,8 @@ import UIKit
 final class Colors {
     static let background_black = UIColor.init(hex: "#000000")
     static let text_white = UIColor.init(hex: "#ffffff")
-    static let text_notice = UIColor.init(hex: "#E8625B")
+    static let text_notice_red = UIColor.init(hex: "#E8625B")
+    static let text_notice_green = UIColor.init(hex: "#7ED29B")
     static let brand_lemon = UIColor.init(hex: "#FBF89B")
     static let brand_light_lemon = UIColor.init(hex: "#FDFBC4")
     static let brand_ivory = UIColor.init(hex: "#FFFFE8")
