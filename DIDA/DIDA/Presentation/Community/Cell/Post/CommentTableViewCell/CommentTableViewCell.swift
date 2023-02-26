e2934d838ea35871095e45caeb75b7b098f508c8
hee <kkll135@naver.com>
2023-02-26 22:33:13 +0900

    Feat(Community): 최신 게시물 섹션 UI 틀 구현

---
+++ DIDA/DIDA/Presentation/Community/Cell/Post/CommentTableViewCell/CommentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentTableViewCell.swift
+//  DIDA
+//
+//  Created by HeecheolYoon on 2023/02/26.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ DIDA/DIDA/Presentation/Community/Cell/Post/PostTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  PostTableViewCell.swift
+//  DIDA
+//
+//  Created by HeecheolYoon on 2023/02/26.
+//
+
+import UIKit
+
+final class PostTableViewCell: UITableViewCell {
+
+    static let identifier = "PostTableViewCell"
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var postTitleLabel: UILabel!
+    @IBOutlet weak var postBodyLabel: UILabel!
+    @IBOutlet weak var nftImageView: UIImageView!
+    @IBOutlet weak var nftNameLabel: UILabel!
+    @IBOutlet weak var coinImageView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var commentTableView: UITableView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
+
+extension PostTableViewCell {
+    private func configure() {
+    }
+}
