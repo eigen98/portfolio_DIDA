324077809b03b61e7b8256eeb050ad8edca5af7d
doitduri <kdle_@naver.com>
2023-02-27 20:57:14 +0900

    Comment: 미사용 주석 제거

--- DIDA/DIDA-UI/TabBar/TabBarController.swift
+++ DIDA/DIDA-UI/TabBar/TabBarController.swift
@@ -21,16 +21,6 @@ class TabBarController: UIViewController {
     func setTabBar() {
         self.tabBar.tabItems = ["좋아요한 NFT", "my NFT"]
         self.tabBar.delegate = self
-        
-        for family in UIFont.familyNames {
-
-                    let sName: String = family as String
-                    print("family: \(sName)")
-                            
-                    for name in UIFont.fontNames(forFamilyName: sName) {
-                        print("name: \(name as String)")
-                    }
-                }
     }
 }
 
