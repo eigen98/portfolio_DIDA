971447776a74a73cb5941f678395d7669f8ad809
eigen98 <terrantrace@gmail.com>
2023-03-26 23:51:48 +0900

    Feat(Home) : MainPage 화면의 데이터와 상태를 관리하는 뷰모델을 구현하였습니다. 1. HomeRepository를 통해 홈화면 데이터를 가져옵니다.

--- DIDA/DIDA/Domain/Usecase/Home/HomeViewModel.swift
+++ DIDA/DIDA/Domain/Usecase/Home/HomeViewModel.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+class HomeViewModel : BaseViewModel{
+    
+    private var disposeBag : DisposeBag
+    
+    struct Input {
+        
+    }
+    struct Output{
+        var homeOutput : BehaviorSubject<HomeEntity>
+    }
+    struct Dependency {
+        let homeRepository : HomeRepository
+    }
+    
+    init(dependency : Dependency) {
+        
+        input = Input()
+        output = Output(homeOutput: BehaviorSubject<HomeEntity>(value: HomeEntity(getHotItems: [],
+                                                                                  getHotSellers: [],
+                                                                                  getRecentCards: [],
+                                                                                  getHotUsers: [])) )
+        self.dependency = dependency
+        disposeBag = DisposeBag()
+    }
+    
+    var input: Input
+    var output: Output
+    var dependency : Dependency
+    
+    override func bind(){
+        dependency.homeRepository
+            .getMain()
+            .map{ response in
+                response.toDomain()
+            }
+            .asObservable()
+            .subscribe(onNext: { result in
+                print("통신 결과 :  \(result)")
+                self.output.homeOutput.onNext(result)
+                
+                
+            }, onCompleted: {
+                print("통신 완료")
+            })
+            .disposed(by: disposeBag)
+        
+        
+    }
+    
+    
+    
+    
+}
+
--- DIDA/DIDA/Network/EndPoints/HomeAPI.swift
+++ DIDA/DIDA/Network/EndPoints/HomeAPI.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Moya
 
+//Home에서 사용할 API 엔드포인트를 정의
 enum HomeAPI{
     case main
     case soldout(term: String)
