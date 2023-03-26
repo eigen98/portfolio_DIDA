b2b3c30c72b7899acd97d915795193154a8d7c15
eigen98 <terrantrace@gmail.com>
2023-03-26 23:56:09 +0900

    Feat(Home) : Rx Extension 추가. 1. SDiffableDataSourceSnapshot을 데이터 소스에 바인딩하고 애니메이션으로 적용하는 Binder를 반환하는 snapshot() 함수를 제공 2. ControlEvent<Void> 유형의 tapGesture 변수를 제공하는 UIView 클래스에 대한 확장

--- DIDA/DIDA/Infratruction/Reactive+Extension.swift
+++ DIDA/DIDA/Infratruction/Reactive+Extension.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+extension Reactive where Base: UICollectionViewDiffableDataSource<Int, HomeSectionType> {
+    func snapshot() -> Binder<NSDiffableDataSourceSnapshot<Int, HomeSectionType>> {
+        return Binder(self.base) { dataSource, snapshot in
+            dataSource.apply(snapshot, animatingDifferences: true)
+        }
+    }
+}
+
+//add a tap gesture recognizer to a UIView
+extension Reactive where Base: UIView {
+    var tapGesture: ControlEvent<Void> {
+        let tapRecognizer = UITapGestureRecognizer()
+        base.addGestureRecognizer(tapRecognizer)
+
+        let controlEvent = ControlEvent<Void>(events: tapRecognizer.rx.event.map { _ in })
+        return controlEvent
+    }
+}
