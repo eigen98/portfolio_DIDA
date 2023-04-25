6221160fa56ed292374a4515ba8458b7c1c7ad69
eigen98 <terrantrace@gmail.com>
2023-04-25 16:42:32 +0900

    Feat : 활발한 활동 더보기 Response DTO 구현

--- DIDA/DIDA/Data/Response/Home/More/GetMoreActivityResponse.swift
+++ DIDA/DIDA/Data/Response/Home/More/GetMoreActivityResponse.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+//활발한 활동 더보기 DTO
+struct GetMoreActivityResponse : Codable{
+    var userId : Int
+    var name : String
+    var profileUrl : String
+    var cardCnt : Int //var count : Int
+    var cardUrls : [String]
+    var followed : Bool
+}
