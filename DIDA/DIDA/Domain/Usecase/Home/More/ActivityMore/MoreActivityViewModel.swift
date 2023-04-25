0f151c9f12dafc14cb66bbc509ef63fdc6e96c4f
eigen98 <terrantrace@gmail.com>
2023-04-25 16:44:37 +0900

    Feat : (활발한활동 더보기) MoreActivityViewModel 구현

--- DIDA/DIDA/Domain/Usecase/Home/More/ActivityMore/MoreActivityViewModel.swift
+++ DIDA/DIDA/Domain/Usecase/Home/More/ActivityMore/MoreActivityViewModel.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class MoreActivityViewModel : BaseViewModel{
+    var disposeBag : DisposeBag
+    var input: Input
+    var output: Output
+    private let moreRepository: MoreHomeRepository
+    
+    struct Input {
+        
+    }
+    
+    struct Output{
+        var activityData : BehaviorSubject<[MoreActivityEntity]>
+    }
+    
+    override init() {
+        input = Input()
+        output = Output(activityData: BehaviorSubject<[MoreActivityEntity]>(value: []))
+        disposeBag = DisposeBag()
+        moreRepository = MoreHomeRepositoryImpl()
+    }
+    
+    
+    override func bind() {
+        moreRepository.getMoreHotActivity(page: 0)
+            .map{
+                $0.toDomain()
+            }
+            .asObservable()
+            .bind(onNext: { [weak self] result in
+                    guard let `self` = self else { return }
+                self.output.activityData.onNext(result)
+            }).disposed(by: disposeBag)
+    }
+}
