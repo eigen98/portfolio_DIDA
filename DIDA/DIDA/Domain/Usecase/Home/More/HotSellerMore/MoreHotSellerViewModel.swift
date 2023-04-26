4f8eafcc76acd34f0eb059d139c1a2934534e915
eigen98 <terrantrace@gmail.com>
2023-04-26 16:42:24 +0900

    Feat : (HotSeller 더보기) ViewModel 구현

---
+++ DIDA/DIDA/Domain/Usecase/Home/More/HotSellerMore/MoreHotSellerViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  MoreHotSellerViewModel.swift
+//  DIDA
+//
+//  Created by JeongMin Ko on 2023/04/26.
+//
+
+import Foundation
+import RxSwift
+class MoreHotSellerViewModel : BaseViewModel{
+    var disposeBag = DisposeBag()
+    var input : Input
+    var output : Output
+    
+    struct Input {
+        
+    }
+    
+    struct Output{
+        var hotSellerData : BehaviorSubject<[HotSellerEntity]>
+    }
+    
+    override init() {
+        
+        self.input = Input()
+        self.output = Output(hotSellerData: BehaviorSubject(value: []))
+    }
+    
+    override func bind() {
+        super.bind()
+        //Mock Data
+        output.hotSellerData.onNext([HotSellerEntity(userId: 7, sellerBacground: "NFT의 imgUrl", sellerProfile: "유저의 imgUrl", sellerName: "yoo_jitsu"),
+                                     HotSellerEntity(userId: 7, sellerBacground: "NFT의 imgUrl", sellerProfile: "유저의 imgUrl", sellerName: "yoo_jitsu")])
+    }
+}
---
+++ DIDA/DIDA/Presentation/Home/More/HotSellerMore/Cell/MoreHotSellerCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MoreHotSellerCollectionViewCell.swift
+//  DIDA
+//
+//  Created by JeongMin Ko on 2023/04/26.
+//
+
+import UIKit
+
+class MoreHotSellerCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
