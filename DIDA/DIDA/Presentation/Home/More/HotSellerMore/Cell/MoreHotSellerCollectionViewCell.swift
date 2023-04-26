ddea968d4e3a608babaaf4e51a5aa12071b778a6
eigen98 <terrantrace@gmail.com>
2023-04-26 16:43:37 +0900

    Feat : (HotSeller 더보기) 더보기 리스트 CollectionView Cell 구현

--- DIDA/DIDA/Presentation/Home/More/HotSellerMore/Cell/MoreHotSellerCollectionViewCell.swift
+++ DIDA/DIDA/Presentation/Home/More/HotSellerMore/Cell/MoreHotSellerCollectionViewCell.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class MoreHotSellerCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var nftCountLabel: UILabel!
+    
+    @IBOutlet weak var followerCountLabel: UILabel!
+    
+    @IBOutlet weak var followingCountLabel: UILabel!
+    
+    @IBOutlet weak var firstNFTImageView: UIImageView!
+    
+    @IBOutlet weak var secondNFTImageView: UIImageView!
+    
+    @IBOutlet weak var thirdNFTImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    func configure(item : HotSellerEntity){
+       
+    }
+    
+    
+    
 
 }
