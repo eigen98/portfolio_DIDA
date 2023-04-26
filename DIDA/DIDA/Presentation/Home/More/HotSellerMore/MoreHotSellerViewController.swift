ab3794d7c5dba9a0f0f7cdf4ffe1e62609427a1f
eigen98 <terrantrace@gmail.com>
2023-04-26 16:45:18 +0900

    Feat : (HotSeller 더보기) 화면 UI 구현, ViewController 구현

--- DIDA/DIDA/Presentation/Home/More/HotSellerMore/MoreHotSellerViewController.swift
+++ DIDA/DIDA/Presentation/Home/More/HotSellerMore/MoreHotSellerViewController.swift
@@ -6,24 +6,47 @@
 //
 
 import UIKit
+import RxSwift
+//HotSeller 더보기
+class MoreHotSellerViewController: BaseViewController {
 
-class MoreHotSellerViewController: UIViewController {
-
+    var disposeBag = DisposeBag()
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    let viewModel: MoreHotSellerViewModel = MoreHotSellerViewModel()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initCollectionView()
         // Do any additional setup after loading the view.
+        bindViewModel()
+        bindEvent()
+    }
+    
+    override func bindViewModel() {
+        viewModel.output.hotSellerData
+            .bind(to: collectionView.rx.items(cellIdentifier: MoreHotSellerCollectionViewCell.reuseIdentifier, cellType: MoreHotSellerCollectionViewCell.self)) { index, item, cell in
+//                cell.configure(item: item)
+            }
+            .disposed(by: disposeBag)
     }
 
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+}
+//MARK: UI
+extension MoreHotSellerViewController{
+    func initCollectionView(){
+        let layout = UICollectionViewFlowLayout()
+        let width = UIScreen.main.bounds.size.width
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 0
+        layout.itemSize = CGSize(width: width - 32.0 , height: 226)
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
+        self.collectionView.collectionViewLayout = layout
+        
+        collectionView.register(UINib(nibName: MoreHotSellerCollectionViewCell.reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: MoreHotSellerCollectionViewCell.reuseIdentifier)
     }
-    */
-
 }
