bee38eda64af2b4716b57e600145f420e84337d8
eigen98 <terrantrace@gmail.com>
2023-06-27 21:08:01 +0900

    Fix : (Home) SoldOutSectionViewModel 리팩토링

--- DIDA/DIDA/Domain/Usecase/Home/SoldOutSectionViewModel.swift
+++ DIDA/DIDA/Domain/Usecase/Home/SoldOutSectionViewModel.swift
@@ -12,7 +12,6 @@ class SoldOutSectionViewModel : BaseViewModel{
     
     private var disposeBag : DisposeBag
     private let homeRepository: HomeRepository
-    private var isLoadingNextPage: Bool = false
     
     var input: Input
     var output: Output
@@ -21,58 +20,44 @@ class SoldOutSectionViewModel : BaseViewModel{
         let refreshTrigger: PublishSubject<String>
     }
     
-    struct Output{
-        var soldoutOutput : BehaviorSubject<[NFTEntity]>
+    struct Output {
+        var soldoutOutput: BehaviorSubject<[NFTEntity]>
         var isLoading: BehaviorSubject<Bool>
         var isRefreshing: BehaviorSubject<Bool>
     }
     
-    
     override init() {
         input = Input(refreshTrigger: PublishSubject<String>())
-        
         output = Output(soldoutOutput: BehaviorSubject<[NFTEntity]>(value: []),
-        
                         isLoading: BehaviorSubject<Bool>(value: false),
                         isRefreshing: BehaviorSubject<Bool>(value: false))
-        
         self.homeRepository = HomeRepositoryImpl()
         disposeBag = DisposeBag()
     }
     
-    override func bind(){
-        
-        let refresh = input.refreshTrigger
-                    .flatMapLatest { term in
-                        self.homeRepository
-                            .getMainSoldout(term: "\(term)" )
-                            .map { response in
-                                response.toDomain()
-                            }
+    override func bind() {
+        input.refreshTrigger
+            .subscribe(onNext: { [weak self] term in
+                self?.homeRepository.getMainSoldout(term: term) { [weak self] result in
+                    switch result {
+                    case .success(let response):
+                        let entities = response.toDomain()
+                        self?.output.soldoutOutput.onNext(entities)
+                    case .failure(let error):
+                        print("Error getting data: \(error)")
                     }
-                    .share()
-        
-        //loadNextPage와 refresh Observable이 반환하는 결과를 합쳐서 soldoutOutput 반영
-        //페이징 및 리프레시 작업이 완료될 때마다 업데이트
-        Observable.merge( refresh)
-                   .subscribe(onNext: { result in
-                       //print("통신 결과 :  \(result)")
-                       print("통신")
-                       self.output.soldoutOutput.onNext(result)
-                   })
-                   .disposed(by: disposeBag)
-        
+                }
+            })
+            .disposed(by: disposeBag)
         
         input.refreshTrigger
             .map { _ in true }
             .bind(to: output.isRefreshing)
             .disposed(by: disposeBag)
         
-        refresh
+        output.soldoutOutput
             .map { _ in false }
             .bind(to: output.isRefreshing)
             .disposed(by: disposeBag)
     }
-    
-    
 }
