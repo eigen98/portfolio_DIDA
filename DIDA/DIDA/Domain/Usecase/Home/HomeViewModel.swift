0289c89c0332419d95a781d4fc22b3420931e9bf
eigen98 <terrantrace@gmail.com>
2023-06-27 21:07:21 +0900

    Fix : (Home) HomeViewModel 리팩토링

--- DIDA/DIDA/Domain/Usecase/Home/HomeViewModel.swift
+++ DIDA/DIDA/Domain/Usecase/Home/HomeViewModel.swift
@@ -9,7 +9,7 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-class HomeViewModel : BaseViewModel{
+class HomeViewModel : BaseViewModel {
     
     private var disposeBag : DisposeBag
     
@@ -19,18 +19,17 @@ class HomeViewModel : BaseViewModel{
         let refreshTrigger: PublishSubject<Void>
     }
     
-    struct Output{
-        var homeOutput : BehaviorSubject<HomeEntity>
-        
+    struct Output {
+        var homeOutput : BehaviorSubject<Result<HomeEntity, Error>>
     }
     
     override init() {
         input = Input(refreshTrigger: PublishSubject<Void>())
         
-        output = Output(homeOutput: BehaviorSubject<HomeEntity>(value: HomeEntity(getHotItems: [],
-                                                                                  getHotSellers: [],
-                                                                                  getRecentCards: [],
-                                                                                  getHotUsers: [])))
+        output = Output(homeOutput: BehaviorSubject<Result<HomeEntity, Error>>(value: .success(HomeEntity(getHotItems: [],
+                                                                                                           getHotSellers: [],
+                                                                                                           getRecentCards: [],
+                                                                                                           getHotUsers: []))))
         
         self.homeRepository = HomeRepositoryImpl()
         disposeBag = DisposeBag()
@@ -39,49 +38,40 @@ class HomeViewModel : BaseViewModel{
     var input: Input
     var output: Output
     
-    override func bind(){
-        let refresh = input.refreshTrigger
-                .flatMapLatest { [weak self] _ -> Observable<HomeEntity> in
-                    guard let self = self else {
-                        return Observable.empty()
-                    }
-                    return self.fetchHomeData()
-                }
-                .share()
-            
-            refresh
-                .subscribe(onNext: {[weak self] result in
-                    self?.output.homeOutput.onNext(result)
-                })
-                .disposed(by: disposeBag)
-            
-            input.refreshTrigger
-                .map { _ in true }
-                .bind(to: showLoading)
-                .disposed(by: disposeBag)
-            
-        //MARK: 로딩 완료
-            refresh
-                .map { _ in false }
-                .bind(to: showLoading)
-                .disposed(by: disposeBag)
-
+    override func bind() {
+        input.refreshTrigger
+            .flatMapLatest { [weak self] _ in
+                self?.fetchHomeData() ?? Observable.empty()
+            }.bind(to: output.homeOutput)
+            .disposed(by: disposeBag)
+        
+        input.refreshTrigger
+            .map { _ in true }
+            .bind(to: showLoading)
+            .disposed(by: disposeBag)
+        
+        output.homeOutput
+            .map { _ in false }
+            .bind(to: showLoading)
+            .disposed(by: disposeBag)
     }
     
     /*
      메인화면 데이터 가져오기
      */
-    private func fetchHomeData() -> Observable<HomeEntity> {
-        return self.homeRepository.getMain()
-                .map { response in
-                    response.toDomain()
+    private func fetchHomeData() -> Observable<Result<HomeEntity, Error>> {
+        return Observable.create { [weak self] observer in
+            self?.homeRepository.getMain() { result in
+                switch result {
+                case .success(let response):
+                    let homeEntity = response.toDomain()
+                    observer.onNext(.success(homeEntity))
+                    
+                case .failure(let error):
+                    observer.onNext(.failure(error))
                 }
-                .asObservable()
-                .catch { error in
-                    self.showError.onNext(error)
-                    return Observable.empty()
-                }
+            }
+            return Disposables.create()
         }
-  
+    }
 }
-
