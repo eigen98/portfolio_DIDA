7d803bde7efd1c41644a2fe5dd0fcf50057c6f2e
eigen98 <terrantrace@gmail.com>
2023-06-28 16:23:46 +0900

    Fix : (Home) MoreRecentNFTViewModel 리팩토링 (데이터 처리 방식 변경)

--- DIDA/DIDA/Domain/Usecase/Home/More/RecentNFTMore/MoreRecentNFTViewModel.swift
+++ DIDA/DIDA/Domain/Usecase/Home/More/RecentNFTMore/MoreRecentNFTViewModel.swift
@@ -9,40 +9,39 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-class MoreRecentNFTViewModel : BaseViewModel{
+
+class MoreRecentNFTViewModel : BaseViewModel {
     var disposeBag : DisposeBag
     var input: Input
     var output: Output
     private let moreRepository: MoreHomeRepository
-    
-    struct Input {
-        
+
+    struct Input {}
+
+    struct Output {
+        var recentNFTData: BehaviorSubject<[NFTEntity]>
     }
-    
-    struct Output{
-        var recentNFTData : BehaviorSubject<[NFTEntity]>
-    }
-    
+
     override init() {
         input = Input()
         output = Output(recentNFTData: BehaviorSubject<[NFTEntity]>(value: []))
         disposeBag = DisposeBag()
         moreRepository = MoreHomeRepositoryImpl()
     }
-    
-    
+
     override func bind() {
         super.bind()
         
-        moreRepository.getMoreRecentNFT(page: 0)
-            .map{
-                $0.toDomain()
+        moreRepository.getMoreRecentNFT(page: 0) { [weak self] result in
+            switch result {
+            case .success(let response):
+                let mappedEntities = response.toDomain()
+                self?.output.recentNFTData.onNext(mappedEntities)
+            case .failure(let error):
+               
+                print(error.localizedDescription)
             }
-            .asObservable()
-            .bind(onNext: { [weak self] result in
-                    guard let `self` = self else { return }
-                self.output.recentNFTData.onNext(result)
-            }).disposed(by: disposeBag)
-        
+        }
     }
 }
+
