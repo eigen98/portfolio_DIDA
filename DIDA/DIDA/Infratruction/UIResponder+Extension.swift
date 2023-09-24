598b3294eeb4371235d3df989141eb1e91ef011a
eigen98 <terrantrace@gmail.com>
2023-09-24 14:54:29 +0900

    feat : UIResponder 체인을 통해 상위 뷰 컨트롤러에 접근하도록 Extension 추가

--- DIDA/DIDA/Infratruction/UIResponder+Extension.swift
+++ DIDA/DIDA/Infratruction/UIResponder+Extension.swift
@@ -5,4 +5,17 @@
 //  Created by JeongMin Ko on 2023/09/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIResponder {
+    var parentViewController: UIViewController? {
+        var nextResponder: UIResponder? = self
+        while nextResponder != nil {
+            if let viewController = nextResponder as? UIViewController {
+                return viewController
+            }
+            nextResponder = nextResponder?.next
+        }
+        return nil
+    }
+}
