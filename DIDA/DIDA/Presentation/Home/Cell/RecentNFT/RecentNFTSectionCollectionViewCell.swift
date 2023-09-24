4c102f62c26e13ccc6fcf9ac05c52d13fb5818c3
eigen98 <terrantrace@gmail.com>
2023-09-24 14:55:06 +0900

    refactor : 최신 NFT 가격 표시 메서드 수정 및 리팩토링

--- DIDA/DIDA/Presentation/Home/Cell/RecentNFT/RecentNFTSectionCollectionViewCell.swift
+++ DIDA/DIDA/Presentation/Home/Cell/RecentNFT/RecentNFTSectionCollectionViewCell.swift
@@ -11,226 +11,142 @@ import RxSwift
 
 
 class RecentNFTSectionCollectionViewCell: UICollectionViewCell {
-
-    var items = [NFTEntity]()
     
+    private enum Constants {
+        static let heartFillImageName = "heart-fill"
+        static let heartUnfillImageName = "heart-unfill"
+        static let defaultDecimalString = "0.00"
+        static let thousandSuffix = "K"
+        static let millionSuffix = "M"
+        static let decimalMaximumFractionDigits = 2
+    }
+    
+    // MARK: - Outlets
     @IBOutlet weak var titleLabel: UILabel!
-    
-    
-    
-    //상단 컨테이너 뷰
     @IBOutlet weak var firstItemsContainerView: UIView!
-    
-    
     @IBOutlet weak var firstContainerView: UIView!
-    
     @IBOutlet weak var firstItemImageView: UIImageView!
-    
     @IBOutlet weak var firstUserNameLabel: UILabel!
-    
     @IBOutlet weak var firstNFTNameLabel: UILabel!
-    
     @IBOutlet weak var firstProfileImageView: UIImageView!
-    
     @IBOutlet weak var firstPriceLabel: UILabel!
-    
-    
     @IBOutlet weak var firstLikeButton: UIButton!
-    
-    
     @IBOutlet weak var secondItemImageView: UIImageView!
-    
     @IBOutlet weak var secondContainerView: UIView!
-    
     @IBOutlet weak var secondUserNameLabel: UILabel!
-    
     @IBOutlet weak var secondProfileImageView: UIImageView!
-    
     @IBOutlet weak var secondPriceLabel: UILabel!
-    
     @IBOutlet weak var secondNFTNameLabel: UILabel!
-    
     @IBOutlet weak var secondLikeButton: UIButton!
-    
-    
-    //하단 컨테이너 뷰
     @IBOutlet weak var secondItemsContainerView: UIView!
-    
     @IBOutlet weak var thirdItemImageView: UIImageView!
-    
     @IBOutlet weak var thirdContainerView: UIView!
-    
     @IBOutlet weak var thirdUserNameLabel: UILabel!
-    
     @IBOutlet weak var thirdNFTNameLabel: UILabel!
-    
     @IBOutlet weak var thirdProfileImageView: UIImageView!
-    
     @IBOutlet weak var thirdPriceLabel: UILabel!
-    
     @IBOutlet weak var thirdLikeButton: UIButton!
-    
-    
     @IBOutlet weak var fourthItemImageView: UIImageView!
-    
     @IBOutlet weak var fourthContainerView: UIView!
-    
     @IBOutlet weak var fourthUserNameLabel: UILabel!
-    
     @IBOutlet weak var fourthNFTNameLabel: UILabel!
-    
     @IBOutlet weak var fourthProfileImageView: UIImageView!
-    
     @IBOutlet weak var fourthPriceLabel: UILabel!
-    
     @IBOutlet weak var fourthLikeButton: UIButton!
-    
-    
     @IBOutlet weak var moreButton: UIButton!
     
+    // MARK: - Properties
     var disposeBag = DisposeBag()
     
+    // MARK: - Lifecycle Methods
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
-    func configure(items : [NFTEntity]){
-        
-        bind()
-        self.items = items
-        
-        // 첫 번째 아이템이 싱글톤 로딩 객체인지 확인
-        if items.first == NFTEntity.loading {
-//            스켈레톤 뷰를 보여줍니다.
-            self.showSkeleton(usingColor: Colors.surface_2!)
-        } else {
-//            스켈레톤 뷰를 숨깁니다.
-            self.hideSkeleton()
-        }
-        
-        let itemViews: [(UIImageView?, UILabel?, UILabel?, UILabel?, UIButton?)] = [
-                (firstItemImageView, firstUserNameLabel, firstNFTNameLabel, firstPriceLabel, firstLikeButton),
-                (secondItemImageView, secondUserNameLabel, secondNFTNameLabel, secondPriceLabel, secondLikeButton),
-                (thirdItemImageView, thirdUserNameLabel, thirdNFTNameLabel, thirdPriceLabel, thirdLikeButton),
-                (fourthItemImageView, fourthUserNameLabel, fourthNFTNameLabel, fourthPriceLabel, fourthLikeButton)
-            ]
-            // 아이템 이미지 로드
-            for (index, item) in items.enumerated() {
-                if let url = URL(string: item.nftImg) {
-                    itemViews[index].0?.kf.setImage(with: url)
-                }
-                // 아이템의 텍스트 필드 설정
-                itemViews[index].1?.text = item.nickname
-                itemViews[index].2?.text = item.nftName
-                itemViews[index].3?.text = "\(roundedStringToTwoDecimalPlaces(value: (item.price))) dida"
-                //좋아요 여부에 따라 설정
-                let imageName = item.liked ? "heart-fill" : "heart-unfill"
-                itemViews[index].4?.setImage(UIImage(named: imageName), for: .normal)
-            }
-        
-        
-        
-    }
     override func prepareForReuse() {
         super.prepareForReuse()
         disposeBag = DisposeBag()
     }
     
-    
-    //스켈레톤 로딩뷰 보여주기
-    func configureLoadingView(){
-        [titleLabel,
-         firstContainerView,
-         secondContainerView,
-         thirdContainerView,
-         fourthContainerView,
-         moreButton]
-            .forEach{
-                $0.startSkeletonAnimation(cornerRadius: 8)
+    // MARK: - Configuration
+    func configure(items: [NFTEntity]) {
+        bind()
+        
+        let containerViews = [firstContainerView, secondContainerView, thirdContainerView, fourthContainerView]
+        let itemViews: [(UIImageView?, UILabel?, UILabel?, UILabel?, UIButton?)] = [
+            (firstItemImageView, firstUserNameLabel, firstNFTNameLabel, firstPriceLabel, firstLikeButton),
+            (secondItemImageView, secondUserNameLabel, secondNFTNameLabel, secondPriceLabel, secondLikeButton),
+            (thirdItemImageView, thirdUserNameLabel, thirdNFTNameLabel, thirdPriceLabel, thirdLikeButton),
+            (fourthItemImageView, fourthUserNameLabel, fourthNFTNameLabel, fourthPriceLabel, fourthLikeButton)
+        ]
+        
+        for (index, containerView) in containerViews.enumerated() {
+            containerView?.isHidden = index >= items.count
+        }
+        
+        for (index, item) in items.enumerated() {
+            if index >= itemViews.count { break }
+            if let url = URL(string: item.nftImg) {
+                itemViews[index].0?.kf.setImage(with: url)
             }
+            itemViews[index].1?.text = item.nickname
+            itemViews[index].2?.text = item.nftName
+            itemViews[index].3?.text = "\(roundedStringToTwoDecimalPlaces(value: (item.price)))"
+            let imageName = item.liked ? Constants.heartFillImageName : Constants.heartUnfillImageName
+            itemViews[index].4?.setImage(UIImage(named: imageName), for: .normal)
+        }
     }
     
-    func removeLottieAnimationView(){
-        [titleLabel,
-         firstContainerView,
-         secondContainerView,
-         thirdContainerView,
-         fourthContainerView,
-         moreButton]
-            .forEach{
-                $0.stopSkeletonAnimation()
-            }
-        moreButton.backgroundColor = .init(hex: "#33333333")
-    }
-    
-
-}
-
-extension RecentNFTSectionCollectionViewCell{
-    func bind(){
+    // MARK: - Binding
+    private func bind() {
         moreButton.rx.tap
             .asDriver(onErrorJustReturn: ())
-            .drive(onNext: {
-                print("tap()")
-                var nextResponder = self.next
-                while nextResponder != nil {
-                    if let viewController = nextResponder as? UIViewController {
-                        print("viewController : \(viewController)")
-                        if let navController = viewController.navigationController {
-                            print("navi : \(navController)")
-                            let nextVC = MoreRecentNFTViewController()
-                            navController.pushViewController(nextVC, animated: true)
-                            break
-                        }
-                    }
-                    nextResponder = nextResponder?.next
-                }
+            .drive(onNext: { [weak self] in
+                guard let viewController = self?.parentViewController,
+                      let navController = viewController.navigationController else { return }
+                
+                let nextVC = MoreRecentNFTViewController()
+                navController.pushViewController(nextVC, animated: true)
             })
             .disposed(by: disposeBag)
         
         let likeButtons = [firstLikeButton, secondLikeButton, thirdLikeButton, fourthLikeButton]
 
-        for (index, likeButton) in likeButtons.enumerated() {
+        for likeButton in likeButtons {
             likeButton?.rx.tap
-                .map { [weak self] in
-                    let isLiked = !(self?.items[index].liked ?? false)
-                    self?.items[index].liked = isLiked
-                    return isLiked
-                }
-                .do(onNext: { isLiked in
-                    let imageName = isLiked ? "heart-fill" : "heart-unfill"
-                    likeButton?.setImage(UIImage(named: imageName), for: .normal)
+                .subscribe(onNext: { [weak self, weak likeButton] in
+                    guard let self = self, let likeButton = likeButton else { return }
+                    let isLiked = likeButton.isSelected
+                    likeButton.isSelected = !isLiked
+                    let imageName = !isLiked ? Constants.heartFillImageName : Constants.heartUnfillImageName
+                    likeButton.setImage(UIImage(named: imageName), for: .normal)
                 })
-                .subscribe()
                 .disposed(by: disposeBag)
         }
-        
     }
     
-    /*
-     String 값을 소수점 둘째 자리 수까지 반올림하는 함수.
-     */
-    func roundedStringToTwoDecimalPlaces(value: String) -> String {
-        if value == "" { return "0.00" }
-            if let doubleValue = Double(value) {
-                var resultValue = ""
-                let absValue = abs(doubleValue)
-                let sign = doubleValue < 0 ? "-" : ""
-                let formatter = NumberFormatter()
-                formatter.numberStyle = .decimal
-                formatter.maximumFractionDigits = 2
-                if absValue < 1000 {
-                    resultValue = formatter.string(from: NSNumber(value: absValue)) ?? ""
-                } else if absValue < 1000000 {
-                    resultValue = formatter.string(from: NSNumber(value: absValue / 1000)) ?? ""
-                    resultValue += "K"
-                } else {
-                    resultValue = formatter.string(from: NSNumber(value: absValue / 1000000)) ?? ""
-                    resultValue += "M"
-                }
-                return "\(sign)\(resultValue)"
-            }
-            return "0.00"
+    // MARK: - Helper Methods
+    private func roundedStringToTwoDecimalPlaces(value: String) -> String {
+        if value.isEmpty { return Constants.defaultDecimalString }
+        
+        if let doubleValue = Double(value) {
+            let absValue = abs(doubleValue)
+            let sign = doubleValue < 0 ? "-" : ""
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            formatter.maximumFractionDigits = Constants.decimalMaximumFractionDigits
+            
+            let (divisor, suffix): (Double, String) = {
+                if absValue < 1_000 { return (1, "") }
+                if absValue < 1_000_000 { return (1_000, Constants.thousandSuffix) }
+                return (1_000_000, Constants.millionSuffix)
+            }()
+            
+            let formattedValue = formatter.string(from: NSNumber(value: absValue / divisor)) ?? ""
+            return "\(sign)\(formattedValue)\(suffix) dida"
+        } else {
+            return value
+        }
     }
 }
