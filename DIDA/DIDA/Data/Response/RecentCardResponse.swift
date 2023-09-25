760dc9f4eb302487effed6ed9190120924a6b800
eigen98 <terrantrace@gmail.com>
2023-09-25 19:43:20 +0900

    feat : 최신 NFT 더보기 API Reponse 변경사항 반영

--- DIDA/DIDA/Data/Response/RecentCardResponse.swift
+++ DIDA/DIDA/Data/Response/RecentCardResponse.swift
@@ -6,12 +6,25 @@
 //
 
 import Foundation
-struct RecentCardResponse: Codable{
-    var nftId : Int?
-    var nftName : String?
-    var memberName : String?
-    var nftImgUrl : String?
-    var price : String?
-    var liked : Bool?
+struct PagedRecentCardResponse: Codable {
+    var page: Int
+    var pageSize: Int
+    var hasNext: Bool
+    var items: [RecentCardResponse]
     
+    enum CodingKeys: String, CodingKey {
+        case page
+        case pageSize
+        case hasNext
+        case items = "response"
+    }
+}
+
+struct RecentCardResponse: Codable {
+    var nftId: Int?
+    var nftName: String?
+    var memberName: String?
+    var nftImgUrl: String?
+    var price: String?
+    var liked: Bool?
 }
