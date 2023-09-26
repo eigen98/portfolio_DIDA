ba892011568a42339705932918e661771762fea2
eigen98 <terrantrace@gmail.com>
2023-09-26 20:36:39 +0900

    feat: UIViewController Extension에 LoadingBlocker 메서드 추가
    - showLoadingBlocker() 메서드를 추가하여, 로딩 중일 때 유저 인터랙션을 차단하는 뷰를 보여줄 수 있습니다.
    - hideLoadingBlocker() 메서드를 추가하여, 로딩이 끝나면 차단 뷰를 숨길 수 있습니다.
    - UIWindowScene을 사용하여 현재 활성화된 window를 찾아 뷰를 추가하거나 제거합니다.

--- DIDA/DIDA/Infratruction/UIViewController+Extension.swift
+++ DIDA/DIDA/Infratruction/UIViewController+Extension.swift
@@ -11,4 +11,18 @@ extension UIViewController {
     func topViewController() -> UIViewController? {
         return self.navigationController?.topViewController
     }
+
+    func showLoadingBlocker() {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let window = windowScene.windows.first(where: { $0.isKeyWindow }) else {
+            return
+        }
+        
+        LoadingBlockerView.shared.frame = window.bounds
+        window.addSubview(LoadingBlockerView.shared)
+    }
+
+    func hideLoadingBlocker() {
+        LoadingBlockerView.shared.removeFromSuperview()
+    }
 }
