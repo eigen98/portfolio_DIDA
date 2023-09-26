01cb03e80c9fb0c0bcf05df5f4f966b26bf7b9bd
eigen98 <terrantrace@gmail.com>
2023-09-26 14:29:21 +0900

    feat : GetMoreSoldOutResponse 모델 추가(Soldout 더보기) Sold Out 더보기 API에 대한 응답 모델 추가

--- DIDA/DIDA/Data/Response/Home/More/GetMoreSoldOutResponse.swift
+++ DIDA/DIDA/Data/Response/Home/More/GetMoreSoldOutResponse.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct GetMoreSoldOutResponse: Codable {
+    let page: Int
+    let pageSize: Int
+    let hasNext: Bool
+    let response: [SoldOutItem]
+    
+    struct SoldOutItem: Codable {
+        let nftInfo: NFTInfo
+        let memberInfo: MemberInfo
+        
+        struct NFTInfo: Codable {
+            let nftId: Int
+            let nftName: String
+            let nftImgUrl: String
+            let price: String
+        }
+        
+        struct MemberInfo: Codable {
+            let memberId: Int
+            let memberName: String
+            let profileImgUrl: String?
+        }
+    }
+}
+
