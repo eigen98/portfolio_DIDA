5381167b1392bc264b130c66cf113789bd2a8afa
eigen98 <terrantrace@gmail.com>
2023-09-26 20:35:35 +0900

    feat: MoreSoldoutViewModel 구현 및 바인딩
    - MoreSoldoutViewModel 클래스를 구현하여 soldout NFT 데이터를 가져오고 관리합니다.
    - Input 구조체에는 refreshTrigger와 rangeTrigger를 정의하여, ViewModel 외부에서 ViewModel을 컨트롤 할 수 있게 하였습니다.
    - Output 구조체에서는 soldoutNFTData를 BehaviorSubject로 정의하여, soldout NFT 데이터의 상태를 구독할 수 있게 하였습니다.
    - bind() 메서드를 override하여 input과 output을 바인딩하였습니다.
    - getSoldoutNFTData(range:page:) 메서드를 통해 MoreHomeRepository를 사용하여 데이터를 가져옵니다.

--- DIDA/DIDA/Domain/Usecase/Home/More/SoldoutMore/MoreSoldoutViewModel.swift
+++ DIDA/DIDA/Domain/Usecase/Home/More/SoldoutMore/MoreSoldoutViewModel.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class MoreSoldoutViewModel: BaseViewModel {
+    
+    struct Input {
+        let refreshTrigger: PublishSubject<Void>
+        let rangeTrigger: PublishSubject<Int>
+    }
+    
+    struct Output {
+        var soldoutNFTData: BehaviorSubject<[NFTEntity]>
+    }
+    
+    var input: Input
+    var output: Output
+    
+    private var disposeBag: DisposeBag
+    private let moreRepository: MoreHomeRepository
+    
+    override init() {
+        input = Input(refreshTrigger: PublishSubject<Void>(),
+                      rangeTrigger: PublishSubject<Int>())
+        output = Output(soldoutNFTData: BehaviorSubject<[NFTEntity]>(value: []))
+        disposeBag = DisposeBag()
+        moreRepository = MoreHomeRepositoryImpl()
+    }
+    
+    override func bind() {
+
+        Observable
+                .combineLatest(input.refreshTrigger, input.rangeTrigger.startWith(7))
+                .do(onNext: { [weak self] _, range in
+                    self?.showLoading.accept(true)
+                })
+                .flatMapLatest { [weak self] _, range in
+                    return self?.getSoldoutNFTData(range: range, page: 0) ?? Observable.empty()
+                }
+                .do(onNext: { [weak self] _ in
+                    self?.showLoading.accept(false)
+                })
+                .bind(onNext: { [weak self] data in
+                    self?.output.soldoutNFTData.onNext(data)
+                })
+                .disposed(by: disposeBag)
+                       
+    }
+
+    private func getSoldoutNFTData(range: Int, page: Int) -> Observable<[NFTEntity]> {
+        return Observable.create { [weak self] observer in
+            self?.moreRepository.getMoreSoldOut(range: range, page: page) { result in
+                switch result {
+                case .success(let response):
+                    observer.onNext(response.toDomain())
+                    observer.onCompleted()
+                case .failure(let error):
+                    observer.onError(error)
+                }
+            }
+            return Disposables.create()
+        }
+    }
+}
