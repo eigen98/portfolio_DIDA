c44cef988e425325a3cb9ced905982f51a777e73
eigen98 <terrantrace@gmail.com>
2023-09-26 20:29:52 +0900

    feat: LoadingBlockerView 추가
    - 로딩 중 다른 UI 상호작용을 막기 위해 LoadingBlockerView를 구현하였습니다.
    - 이 뷰는 앱 내에서 공유되는 Singleton 인스턴스로서 사용됩니다.
    - UIViewController의 Extension을 통해 LoadingBlockerView를 보여주고, 숨길 수 있는 메서드를 추가하였습니다.

---
+++ DIDA/DIDA/Presentation/Home/More/SoldoutMore/Cell/SoldoutCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SoldoutCollectionViewCell.swift
+//  DIDA
+//
+//  Created by JeongMin Ko on 2023/09/26.
+//
+
+import UIKit
+
+class SoldoutCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
