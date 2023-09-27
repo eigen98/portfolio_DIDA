9f551fdd88d0372390ff1dd5e8a4de61dbabcd61
eigen98 <terrantrace@gmail.com>
2023-09-27 14:50:24 +0900

    feat: 활발한 활동 더보기 뷰모델 로딩 상태 처리 로직 추가

--- DIDA/DIDA/Domain/Usecase/Home/More/ActivityMore/MoreActivityViewModel.swift
+++ DIDA/DIDA/Domain/Usecase/Home/More/ActivityMore/MoreActivityViewModel.swift
@@ -10,36 +10,60 @@ import RxSwift
 import RxCocoa
 
 class MoreActivityViewModel : BaseViewModel {
-    var disposeBag : DisposeBag
+    var disposeBag: DisposeBag
     var input: Input
     var output: Output
     private let moreRepository: MoreHomeRepository
-
-    struct Input {}
-
+    
+    struct Input {
+        let refreshTrigger: PublishRelay<Void>
+    }
+    
     struct Output {
         var activityData: BehaviorSubject<[MoreActivityEntity]>
     }
-
+    
     override init() {
-        input = Input()
+        input = Input(refreshTrigger: PublishRelay<Void>())
         output = Output(activityData: BehaviorSubject<[MoreActivityEntity]>(value: []))
         disposeBag = DisposeBag()
         moreRepository = MoreHomeRepositoryImpl()
+        super.init()
+        bind()
     }
-
+    
     override func bind() {
         super.bind()
         
-        moreRepository.getMoreHotActivity(page: 0) { [weak self] result in
-            switch result {
-            case .success(let response):
-                let mappedEntities = response.toDomain()
-                self?.output.activityData.onNext(mappedEntities)
-            case .failure(let error):
-              
-                print(error.localizedDescription)
+        input.refreshTrigger
+            .do(onNext: { [weak self] _ in
+                self?.showLoading.accept(true)
+            })
+            .flatMapLatest { [weak self] _ in
+                self?.getMoreActivityData() ?? Observable.empty()
             }
+            .delay(.seconds(3), scheduler: MainScheduler.instance)
+            .do(onNext: { [weak self] _ in
+                self?.showLoading.accept(false)
+            })
+            .bind(onNext: { [weak self] data in
+                self?.output.activityData.onNext(data)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func getMoreActivityData() -> Observable<[MoreActivityEntity]> {
+        return Observable.create { [weak self] observer in
+            self?.moreRepository.getMoreHotActivity(page: 0) { result in
+                switch result {
+                case .success(let response):
+                    let mappedEntities = response.response.toDomain()
+                    observer.onNext(mappedEntities)
+                case .failure(let error):
+                    observer.onError(error)
+                }
+            }
+            return Disposables.create()
         }
     }
 }
