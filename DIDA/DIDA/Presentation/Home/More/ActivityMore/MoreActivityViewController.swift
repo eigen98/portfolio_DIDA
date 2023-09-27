1e19b8839e100d1ff2816b8294f4df9d73a03ce8
eigen98 <terrantrace@gmail.com>
2023-09-27 15:16:19 +0900

    feat: 활발한 활동 더보기 뷰 컨트롤러 구현

--- DIDA/DIDA/Presentation/Home/More/ActivityMore/MoreActivityViewController.swift
+++ DIDA/DIDA/Presentation/Home/More/ActivityMore/MoreActivityViewController.swift
@@ -18,31 +18,67 @@ class MoreActivityViewController: BaseViewController {
         super.viewDidLoad()
 
         
+        setupTitleBar()
         initCollectionView()
         bindEvent()
         bindViewModel()
+        viewModel.input.refreshTrigger.accept(())
     }
     
-    override func bindViewModel() {
-        viewModel.output.activityData
-            .bind(to: collectionView.rx.items(cellIdentifier: MoreActivityCollectionViewCell.reuseIdentifier, cellType: MoreActivityCollectionViewCell.self)) { index, item, cell in
-                cell.configure(item: item)
-            }
+    override func bindEvent() {
+        collectionView.rx.modelSelected(MoreActivityEntity.self)
+            .subscribe(onNext: { item in
+               
+            })
+            .disposed(by: disposeBag)
+        
+        collectionView.refreshControl?.rx.controlEvent(.valueChanged)
+            .bind(to: viewModel.input.refreshTrigger)
             .disposed(by: disposeBag)
     }
+    
+    private func setupTitleBar() {
+           self.setupBackButton()
+           customTitleBar.title = "활발한 활동"
+       }
+       
+       override func bindViewModel() {
+           viewModel.output.activityData
+               .bind(to: collectionView.rx.items(cellIdentifier: MoreActivityCollectionViewCell.reuseIdentifier, cellType: MoreActivityCollectionViewCell.self)) { index, item, cell in
+                   cell.configure(item: item)
+               }
+               .disposed(by: disposeBag)
+           
+           viewModel.showLoading
+               .bind(onNext: { [weak self] isLoading in
+                   if isLoading {
+                       self?.showLoadingBlocker()
+                       self?.viewModel.output.activityData.onNext([.loading, .loading, .loading])
+                   } else {
+                       self?.hideLoadingBlocker()
+                       self?.collectionView.refreshControl?.endRefreshing()
+                   }
+               })
+               .disposed(by: disposeBag)
+       }
+   }
 
-}
-//MARK: UI
+// MARK: - UI
 extension MoreActivityViewController{
-    func initCollectionView(){
+    func initCollectionView() {
         let layout = UICollectionViewFlowLayout()
         let width = UIScreen.main.bounds.size.width
         layout.minimumLineSpacing = 10
         layout.minimumInteritemSpacing = 0
-        layout.itemSize = CGSize(width: width - 32.0 , height: 230)
+        layout.itemSize = CGSize(width: width - 32.0, height: 230)
         layout.sectionInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
-        self.collectionView.collectionViewLayout = layout
+        collectionView.collectionViewLayout = layout
         
         collectionView.register(UINib(nibName: MoreActivityCollectionViewCell.reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: MoreActivityCollectionViewCell.reuseIdentifier)
+        
+        let refreshControl = UIRefreshControl()
+        collectionView.refreshControl = refreshControl
     }
+    
+    
 }
