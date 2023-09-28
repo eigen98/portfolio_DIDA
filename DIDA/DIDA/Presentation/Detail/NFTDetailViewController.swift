c99a01470c6f065307037c447badcfaf84b968ab
eigen98 <terrantrace@gmail.com>
2023-09-28 12:42:17 +0900

    feat: NFT 상세보기 뷰 컨트롤러, xib 파일 생성

---
+++ DIDA/DIDA/Presentation/Detail/NFTDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NFTDetailViewController.swift
+//  DIDA
+//
+//  Created by JeongMin Ko on 2023/09/28.
+//
+
+import UIKit
+
+class NFTDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
