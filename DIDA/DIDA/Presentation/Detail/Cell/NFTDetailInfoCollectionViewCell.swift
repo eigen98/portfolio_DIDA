b49e16fc331f26763545bc4a27ea85e7240762e6
eigen98 <terrantrace@gmail.com>
2023-09-28 13:21:12 +0900

    feat: NFT 상세정보 Cell 파일, Xib 생성

---
+++ DIDA/DIDA/Presentation/Detail/Cell/NFTDetailInfoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NFTDetailInfoCollectionViewCell.swift
+//  DIDA
+//
+//  Created by JeongMin Ko on 2023/09/28.
+//
+
+import UIKit
+
+class NFTDetailInfoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
