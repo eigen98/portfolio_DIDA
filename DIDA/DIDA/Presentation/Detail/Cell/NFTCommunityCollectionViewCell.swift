84a9dc561778845e6f8ece14704ddd94a41c5f7f
eigen98 <terrantrace@gmail.com>
2023-09-29 21:52:54 +0900

    feat: NFT 상세보기 커뮤니티 셀 구현

--- DIDA/DIDA/Presentation/Detail/Cell/NFTCommunityCollectionViewCell.swift
+++ DIDA/DIDA/Presentation/Detail/Cell/NFTCommunityCollectionViewCell.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class NFTCommunityCollectionViewCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var postButton: Buttons!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupButton()
     }
-
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        hideSkeleton()
+        setupButton()
+    }
+    
+    func configure(with data: CommunityData?) {
+        guard let data = data else {
+            showSkeleton()
+            return
+        }
+        hideSkeleton()
+    }
+    
+    private func setupButton() {
+        postButton.shape = .round
+        postButton.backgroundColor = .white
+        postButton.tintColor = .black
+        let font = UIFont(name: "Pretendard-Bold", size: 16)
+        postButton.titleLabel?.font = font
+    }
+    
 }
