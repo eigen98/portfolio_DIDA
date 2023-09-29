8d025f6c7b5c71d00f743e110cd72c8f3ab9a97f
eigen98 <terrantrace@gmail.com>
2023-09-29 21:52:28 +0900

    feat: NFT 상세 정보 Entity 정의

--- DIDA/DIDA/Domain/Entity/Market/NFTDetailEntity.swift
+++ DIDA/DIDA/Domain/Entity/Market/NFTDetailEntity.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct NFTDetailEntity {
+    let nftId: Int
+    let nftName: String
+    let nftImgUrl: String
+    let price: String
+    let description: String
+    let memberId: Int
+    let memberName: String
+    let profileImgUrl: String
+    let tokenId: String
+    let contractAddress: String
+    let followed: Bool
+    let liked: Bool
+    let isMe: Bool
+}
+
