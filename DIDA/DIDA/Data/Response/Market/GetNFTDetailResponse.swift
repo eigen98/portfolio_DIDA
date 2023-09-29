f27a800732833dbf9e5a3081eaa3f9b27ebb6ada
eigen98 <terrantrace@gmail.com>
2023-09-29 21:51:58 +0900

    feat: NFT 상세보기 API 요청 Response 구현

--- DIDA/DIDA/Data/Response/Market/GetNFTDetailResponse.swift
+++ DIDA/DIDA/Data/Response/Market/GetNFTDetailResponse.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+struct GetNFTDetailResponse: Decodable {
+    let nftInfo: NFTInfo?
+    let description: String?
+    let memberInfo: MemberInfo?
+    let tokenId: String?
+    let contractAddress: String?
+    let followed: Bool?
+    let liked: Bool?
+    let isMe: Bool?
+    
+    struct NFTInfo: Decodable {
+        let nftId: Int?
+        let nftName: String?
+        let nftImgUrl: String?
+        let price: String?
+    }
+    
+    struct MemberInfo: Decodable {
+        let memberId: Int?
+        let memberName: String?
+        let profileImgUrl: String?
+    }
+    
+    func toEntity() -> NFTDetailEntity {
+        return NFTDetailEntity(
+            nftId: nftInfo?.nftId ?? 0,
+            nftName: nftInfo?.nftName ?? "",
+            nftImgUrl: nftInfo?.nftImgUrl ?? "",
+            price: nftInfo?.price ?? "",
+            description: description ?? "",
+            memberId: memberInfo?.memberId ?? 0,
+            memberName: memberInfo?.memberName ?? "",
+            profileImgUrl: memberInfo?.profileImgUrl ?? "",
+            tokenId: tokenId ?? "",
+            contractAddress: contractAddress ?? "",
+            followed: followed ?? false,
+            liked: liked ?? false,
+            isMe: isMe ?? false
+        )
+    }
+
+}
