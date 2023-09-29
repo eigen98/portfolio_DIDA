16cc2dbd0bae98d319dc8880e9243cbe2c3ffa87
eigen98 <terrantrace@gmail.com>
2023-09-29 21:51:33 +0900

    feat: NFT 상세보기 API 요청 메서드 구현

--- DIDA/DIDA/Data/Repositories/implements/MarketRepositoryImpl.swift
+++ DIDA/DIDA/Data/Repositories/implements/MarketRepositoryImpl.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import RxSwift
+
+class MarketRepositoryImpl: MarketRepository {
+    
+    private let disposeBag = DisposeBag()
+    
+    func getNFTDetail(nftId: Int, completion: @escaping (Result<GetNFTDetailResponse, Error>) -> ()) {
+        APIClient.request(.nftDetail(nftId: nftId))
+            .map(GetNFTDetailResponse.self)
+            .subscribe(onSuccess: { (response) in
+                completion(.success(response))
+            }, onFailure: { (error) in
+                completion(.failure(error))
+            }).disposed(by: disposeBag)
+    }
+}
