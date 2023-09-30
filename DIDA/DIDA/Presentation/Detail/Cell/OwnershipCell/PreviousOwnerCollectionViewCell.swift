6f17fd9303f56674880897b47778b9592f2befba
eigen98 <terrantrace@gmail.com>
2023-09-30 12:50:23 +0900

    feat: 이전 소유자 셀 생성

---
+++ DIDA/DIDA/Presentation/Detail/Cell/OwnershipCell/PreviousOwnerCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PreviousOwnerCollectionViewCell.swift
+//  DIDA
+//
+//  Created by JeongMin Ko on 2023/09/30.
+//
+
+import UIKit
+
+class PreviousOwnerCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
