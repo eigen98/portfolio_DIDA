07d6acea7a77b7bc55514b78b4f55bd8b73c4a8d
eigen98 <terrantrace@gmail.com>
2023-09-30 14:16:18 +0900

    feat: 현재 소유자 셀 구현

--- DIDA/DIDA/Presentation/Detail/Cell/OwnershipCell/CurrentOwnerCollectionViewCell.swift
+++ DIDA/DIDA/Presentation/Detail/Cell/OwnershipCell/CurrentOwnerCollectionViewCell.swift
@@ -8,10 +8,39 @@
 import UIKit
 
 class CurrentOwnerCollectionViewCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var containerHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var containerWidthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        let width = UIScreen.main.bounds.size.width
+        containerHeightConstraint.constant = 92
+        containerWidthConstraint.constant = width
+        setupSeparator()
+    }
+    
+    func configure(with owner: Owner) {
+        dateLabel.text = owner.date
+        userNameLabel.text = owner.userName
+        priceLabel.text = owner.price
+    }
+    
+    func setupSeparator() {
+        let separatorHeight: CGFloat = 8.0
+        let separator = CALayer()
+        separator.backgroundColor = UIColor.separator.cgColor
+        separator.frame = CGRect(x: 0, y: self.bounds.height - separatorHeight, width: self.bounds.width, height: separatorHeight)
+        containerView.layer.addSublayer(separator)
     }
 
 }
