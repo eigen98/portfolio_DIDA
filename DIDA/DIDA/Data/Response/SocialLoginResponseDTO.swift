5269b859a02faeaa7c62a0a863c40cc49baf7a9a
eigen98 <terrantrace@gmail.com>
2023-09-30 17:44:36 +0900

    refactor : 로그인 ResponseDTO 통합, FirstLoginResponseDTO 제거

--- DIDA/DIDA/Data/Response/SocialLoginResponseDTO.swift
+++ DIDA/DIDA/Data/Response/SocialLoginResponseDTO.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 struct SocialLoginResponseDTO: Codable {
+    let code: String?
+    let message: String?
+    let timestamp: String?
     let accessToken: String?
     let accessTokenExpirationTime: String?
     let refreshToken: String?
