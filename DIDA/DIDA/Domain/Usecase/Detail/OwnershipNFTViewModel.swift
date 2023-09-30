cdaa71b26da989e8ce273a14c2619d78fb543dff
eigen98 <terrantrace@gmail.com>
2023-09-30 14:15:16 +0900

    feat: 소유자 내역 뷰모델 구현

--- DIDA/DIDA/Domain/Usecase/Detail/OwnershipNFTViewModel.swift
+++ DIDA/DIDA/Domain/Usecase/Detail/OwnershipNFTViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import RxSwift
+import RxDataSources
+
+class OwnershipNFTViewModel {
+    
+    struct Input {
+        let refreshTrigger: Observable<Void>
+    }
+    
+    struct Output {
+        let sections: Observable<[SectionModel<String, Owner>]>
+    }
+    
+    func transform(input: Input) -> Output {
+        let sections = input.refreshTrigger
+            .flatMapLatest { [weak self] in
+                self?.fetchOwnershipData() ?? .just([])
+            }
+        
+        return Output(sections: sections)
+    }
+    
+    private func fetchOwnershipData() -> Observable<[SectionModel<String, Owner>]> {
+      
+        let currentOwner = Owner(date: "2023-09-30", userName: "Current User", price: "$1000")
+        let previousOwner1 = Owner(date: "2023-09-29", userName: "User1", price: "$900")
+        let previousOwner2 = Owner(date: "2023-09-28", userName: "User2", price: "$800")
+        
+        let sections = [
+            SectionModel(model: "현재 소유자", items: [currentOwner]),
+            SectionModel(model: "이전 소유 내역", items: [previousOwner1, previousOwner2])
+        ]
+        
+        return .just(sections)
+    }
+}
