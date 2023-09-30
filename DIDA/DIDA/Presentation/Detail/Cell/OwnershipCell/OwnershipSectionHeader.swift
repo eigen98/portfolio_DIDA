184570f50f69887fbdd77f64758273f143b185e0
eigen98 <terrantrace@gmail.com>
2023-09-30 14:16:58 +0900

    feat: 소유자 내역 헤더 구현

--- DIDA/DIDA/Presentation/Detail/Cell/OwnershipCell/OwnershipSectionHeader.swift
+++ DIDA/DIDA/Presentation/Detail/Cell/OwnershipCell/OwnershipSectionHeader.swift
@@ -5,4 +5,32 @@
 //  Created by JeongMin Ko on 2023/09/30.
 //
 
-import Foundation
+import UIKit
+
+class OwnershipSectionHeader: UICollectionReusableView {
+    var titleLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupTitleLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setupTitleLabel()
+    }
+    
+    private func setupTitleLabel() {
+        titleLabel = UILabel()
+        titleLabel.textAlignment = .center
+        titleLabel.font = Fonts.bold_18
+        titleLabel.textColor = .white
+        addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
