325e62bd2d8aaaa6059e3d6ccfca5f7f2a910029
eigen98 <terrantrace@gmail.com>
2023-10-01 13:35:22 +0900

    feat: RecentNFTSectionViewModel에 NFT 좋아요 기능 Integrate

--- DIDA/DIDA/Domain/Usecase/Home/RecentNFTSectionViewModel.swift
+++ DIDA/DIDA/Domain/Usecase/Home/RecentNFTSectionViewModel.swift
@@ -22,7 +22,7 @@ class RecentNFTSectionViewModel: BaseViewModel {
         let items: BehaviorSubject<[NFTEntity]>
         let likeStatusChanged: PublishSubject<(Bool, Int)>
     }
-    
+    private var repository : SocialInteractionRepository
     private var disposeBag = DisposeBag()
     
     override init() {
@@ -31,19 +31,46 @@ class RecentNFTSectionViewModel: BaseViewModel {
         
         self.input = Input(likeButtonTapped: PublishRelay(), moreButtonTapped: PublishRelay())
         self.output = Output(items: itemsSubject, likeStatusChanged: likeStatusChangedSubject)
-        
+        repository = SocialInteractionRepositoryImpl()
         super.init()
         
     }
     
     override func bind() {
         input.likeButtonTapped.subscribe(onNext: { [weak self] index in
-            guard var viewModels = try? self?.output.items.value() else { return }
-            viewModels[index].liked.toggle()
-            self?.output.items.onNext(viewModels)
-            self?.output.likeStatusChanged.onNext((viewModels[index].liked, index))
+            self?.toggleLikeStatus(at: index)
         }).disposed(by: disposeBag)
+    }
+    
+    private func toggleLikeStatus(at index: Int) {
+        guard var items = try? output.items.value() else { return }
+        items[index].liked.toggle()
+        output.items.onNext(items)
+        output.likeStatusChanged.onNext((items[index].liked, index))
         
+        performLikeAction(for: items[index])
+    }
+    
+    private func performLikeAction(for item: NFTEntity) {
+        repository.likeNFT(nftId: item.cardId) { [weak self] result in
+            switch result {
+            case .success(let success):
+                if !success {
+                    self?.revertLikeStatus(for: item.cardId)
+                }
+            case .failure(let error):
+                self?.revertLikeStatus(for: item.cardId)
+                print("Error liking NFT: \(error)")
+            }
+        }
+    }
+    private func revertLikeStatus(for cardId: Int) {
+        guard var items = try? output.items.value(),
+              let index = items.firstIndex(where: { $0.cardId == cardId }) else { return }
+        
+        items[index].liked.toggle()
+        output.items.onNext(items)
+        output.likeStatusChanged.onNext((items[index].liked, index))
     }
     
     func configure(items: [NFTEntity]) {
