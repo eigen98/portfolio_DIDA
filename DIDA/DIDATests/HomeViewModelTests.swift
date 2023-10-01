6ac793e0943caa85a12611b0af515d76d9d89115
eigen98 <terrantrace@gmail.com>
2023-10-01 23:45:20 +0900

    feat: HomeViewModel 테스트 코드 구현

--- DIDA/DIDATests/HomeViewModelTests.swift
+++ DIDA/DIDATests/HomeViewModelTests.swift
@@ -6,30 +6,130 @@
 //
 
 import XCTest
+@testable import DIDA
+import RxSwift
+import RxTest
 
-final class HomeViewModelTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+#if TEST
+extension HomeViewModel {
+    func testableFetchHomeData() -> Observable<Result<HomeEntity, Error>> {
+        return self.fetchHomeData()
     }
+}
+#endif
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+class HomeViewModelTests: XCTestCase {
+    
+    var viewModel: HomeViewModel!
+    var disposeBag: DisposeBag!
+    var scheduler: TestScheduler!
+    
+    override func setUp() {
+        super.setUp()
+        disposeBag = DisposeBag()
+        scheduler = TestScheduler(initialClock: 0)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    override func tearDown() {
+        viewModel = nil
+        disposeBag = nil
+        scheduler = nil
+        super.tearDown()
     }
+    
+    func testRefreshTrigger() {
+        // Given
+        let mockRepository = MockHomeRepository()
+        viewModel = HomeViewModel(homeRepository: mockRepository)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let loadingObserver = scheduler.createObserver(Bool.self)
+        viewModel.showLoading.bind(to: loadingObserver).disposed(by: disposeBag)
+
+        // When
+        viewModel.input.refreshTrigger.onNext(())
+
+        // Then
+        let expectedLoadingEvents = [Recorded.next(0, true), Recorded.next(0, false)]
+        XCTAssertEqual(loadingObserver.events, expectedLoadingEvents)
+    }
+    
+    func testRepositoryGetMainCalled() {
+        // Given
+        let mockRepository = MockHomeRepository()
+        viewModel = HomeViewModel(homeRepository: mockRepository)
+
+        // When
+        viewModel.input.refreshTrigger.onNext(())
+
+        // Then
+        XCTAssertTrue(mockRepository.getMainCalled)
+    }
+    
+    func testHomeOutputFailure() {
+        // Given
+        let mockRepository = MockHomeRepository()
+        viewModel = HomeViewModel(homeRepository: mockRepository)
+
+        let expectedError = NSError(domain: "Test", code: 0, userInfo: nil)
+        mockRepository.expectedResult = .failure(expectedError)
+
+        let observer = scheduler.createObserver(Result<HomeEntity, Error>.self)
+
+        // When
+        viewModel.output.homeOutput
+            .subscribe(observer)
+            .disposed(by: disposeBag)
+
+        scheduler.start() // TestScheduler 시작
+        viewModel.input.refreshTrigger.onNext(())
+
+        // Then
+        let recordedEvents = observer.events
+        XCTAssertFalse(recordedEvents.isEmpty, "No events recorded")
+        
+        guard recordedEvents.count > 1 else {
+            XCTFail("Expected more than one event due to initial value of BehaviorSubject")
+            return
+        }
+        
+        let recordedEvent = recordedEvents[1]
+        
+        switch recordedEvent.value {
+        case .next(let result):
+            switch result {
+            case .failure(let error as NSError):
+                XCTAssertEqual(error.domain, expectedError.domain)
+                XCTAssertEqual(error.code, expectedError.code)
+            default:
+                XCTFail("Expected failure event")
+            }
+        default:
+            XCTFail("Expected next event")
         }
     }
 
+}
+
+class MockHomeRepository: HomeRepository {
+    
+    var expectedResult: Result<HomeEntity, Error>!
+    var getMainCalled = false
+    func getMain(completion: @escaping (Result<DIDA.GetMainResponse, Error>) -> ()) {
+        getMainCalled = true
+        switch expectedResult {
+        case .success(let homeEntity):
+            let response = DIDA.GetMainResponse()
+            completion(.success(response))
+        case .failure(let error):
+            completion(.failure(error))
+        case .none:
+          
+            break
+        }
+    }
+
+    
+    func getMainSoldout(range: Int, completion: @escaping (Result<GetMainSoldoutNFTResponse, Error>) -> ()) {
+        
+    }
 }
