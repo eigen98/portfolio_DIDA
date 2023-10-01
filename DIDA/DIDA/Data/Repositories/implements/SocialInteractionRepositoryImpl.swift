b20bafb5d488844c9a109186d38c5a4f3b2febf9
eigen98 <terrantrace@gmail.com>
2023-10-01 13:34:55 +0900

    feat: NFT 좋아요 요청 메서드 구현

--- DIDA/DIDA/Data/Repositories/implements/SocialInteractionRepositoryImpl.swift
+++ DIDA/DIDA/Data/Repositories/implements/SocialInteractionRepositoryImpl.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import RxSwift
+
+class SocialInteractionRepositoryImpl: SocialInteractionRepository {
+    
+    private let disposeBag = DisposeBag()
+    
+    func likeNFT(nftId: Int, completion: @escaping (Result<Bool, Error>) -> ()) {
+        APIClient.request(.likeNFT(nftId: nftId))
+            .map { response -> Bool in
+                if response.statusCode == 200 {
+                    return true
+                } else {
+                    let error = try response.map(BaseErrorResponseDTO.self)
+                    throw DidaError.apiError(error)
+                }
+            }
+            .subscribe(onSuccess: { (success) in
+                completion(.success(success))
+            }, onFailure: { (error) in
+                completion(.failure(error))
+            }).disposed(by: disposeBag)
+    }
+}
