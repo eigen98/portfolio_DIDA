e161fc2e93ecbe3cd663ed8e45b972cf1b57a5af
eigen98 <terrantrace@gmail.com>
2023-10-01 13:16:25 +0900

    feat: 최신NFT Cell 뷰모델 구현

--- DIDA/DIDA/Domain/Usecase/Home/RecentNFTSectionViewModel.swift
+++ DIDA/DIDA/Domain/Usecase/Home/RecentNFTSectionViewModel.swift
@@ -5,4 +5,48 @@
 //  Created by JeongMin Ko on 2023/10/01.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+
+class RecentNFTSectionViewModel: BaseViewModel {
+    
+    var input: Input
+    var output: Output
+    
+    struct Input {
+        let likeButtonTapped: PublishRelay<Int>
+        let moreButtonTapped: PublishRelay<Void>
+    }
+    
+    struct Output {
+        let items: BehaviorSubject<[NFTEntity]>
+        let likeStatusChanged: PublishSubject<(Bool, Int)>
+    }
+    
+    private var disposeBag = DisposeBag()
+    
+    override init() {
+        let itemsSubject = BehaviorSubject<[NFTEntity]>(value: [])
+        let likeStatusChangedSubject = PublishSubject<(Bool, Int)>()
+        
+        self.input = Input(likeButtonTapped: PublishRelay(), moreButtonTapped: PublishRelay())
+        self.output = Output(items: itemsSubject, likeStatusChanged: likeStatusChangedSubject)
+        
+        super.init()
+        
+    }
+    
+    override func bind() {
+        input.likeButtonTapped.subscribe(onNext: { [weak self] index in
+            guard var viewModels = try? self?.output.items.value() else { return }
+            viewModels[index].liked.toggle()
+            self?.output.items.onNext(viewModels)
+            self?.output.likeStatusChanged.onNext((viewModels[index].liked, index))
+        }).disposed(by: disposeBag)
+        
+    }
+    
+    func configure(items: [NFTEntity]) {
+        output.items.onNext(items)
+    }
+}
