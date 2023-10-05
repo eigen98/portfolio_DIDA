6c585e01da682d3f61cf7cc76578c0d98e1c6b10
eigen98 <terrantrace@gmail.com>
2023-10-05 23:53:18 +0900

    refactor: Sticky Header 스켈레톤 뷰 로직 수정

--- DIDA/DIDA/Presentation/Home/Cell/TabbarCell/TabbarCollectionReusableView.swift
+++ DIDA/DIDA/Presentation/Home/Cell/TabbarCell/TabbarCollectionReusableView.swift
@@ -13,10 +13,19 @@ class TabbarCollectionReusableView: UICollectionReusableView {
     private var currentTabIndex: Int = 0
     private(set) var tabSelectedSubject = PublishSubject<Int>()
     var isLoading = false
+    private var loadingView: UIView = {
+        var view = UIView()
+        view.skeletonCornerRadius = 12
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.isSkeletonable = true
+        view.isUserInteractionEnabled = false
+       return view
+    }()
     
     lazy var tabbar: ScrollableTabBar = {
         let tabbar = ScrollableTabBar()
         tabbar.width = 102
+        tabbar.isSkeletonable = true
         tabbar.translatesAutoresizingMaskIntoConstraints = false
         return tabbar
     }()
@@ -40,6 +49,7 @@ class TabbarCollectionReusableView: UICollectionReusableView {
 
     private func setupView() {
         addSubview(tabbar)
+        addSubview(loadingView)
         setupConstraints()
         self.backgroundColor = .black
         setTabBar()
@@ -49,9 +59,21 @@ class TabbarCollectionReusableView: UICollectionReusableView {
         NSLayoutConstraint.activate([
             tabbar.leadingAnchor.constraint(equalTo: leadingAnchor),
             tabbar.topAnchor.constraint(equalTo: topAnchor),
-            bottomAnchor.constraint(equalTo: tabbar.bottomAnchor),
-            trailingAnchor.constraint(equalTo: tabbar.trailingAnchor)
+            tabbar.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tabbar.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tabbar.heightAnchor.constraint(equalToConstant: 44),
+            
+            loadingView.leadingAnchor.constraint(equalTo: leadingAnchor ,
+                                                 constant: 16),
+            loadingView.trailingAnchor.constraint(equalTo: trailingAnchor,
+                                                  constant: -16),
+            loadingView.topAnchor.constraint(equalTo: topAnchor,
+                                             constant: 0),
+            loadingView.bottomAnchor.constraint(equalTo: bottomAnchor,
+                                                constant: 0)
+            
         ])
+        
     }
     
     func setTabBar() {
@@ -61,15 +83,17 @@ class TabbarCollectionReusableView: UICollectionReusableView {
     
     //스켈레톤 로딩뷰 보여주기
     func configureLoadingView(){
+        tabbar.isHidden = true
+        loadingView.isHidden = false
+        loadingView.showSkeleton()
         
-        let loadingView = UIView()
-        loadingView.frame = self.frame
-        self.addSubview(loadingView)
-//        loadingView.startSkeletonAnimation()
     }
     
-    func removeLottieAnimationView(){
-        //self.contentView.stopSkeletonAnimation()
+    func removeLoadingView(){
+        tabbar.isHidden = false
+        loadingView.isHidden = true
+        loadingView.hideSkeleton()
+        
     }
 }
 
