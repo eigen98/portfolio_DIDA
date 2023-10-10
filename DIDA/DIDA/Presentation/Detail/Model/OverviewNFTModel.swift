586f8f9a1b0d34e56eec5375bb2dab77a86c2fed
eigen98 <terrantrace@gmail.com>
2023-10-10 15:27:40 +0900

    feat: NFT 상세 OverView Cell 모델 정의

--- DIDA/DIDA/Presentation/Detail/Model/OverviewNFTModel.swift
+++ DIDA/DIDA/Presentation/Detail/Model/OverviewNFTModel.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct OverviewNFTModel {
+    let nftImageUrl : String
+    let nftName: String
+    let description: String
+    let memeberId : Int
+    let memberName: String
+    let memberImageUrl : String
+    let followed: Bool
+}
