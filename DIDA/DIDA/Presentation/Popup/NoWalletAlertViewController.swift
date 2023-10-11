67845313768f245f190cc8e94b92dfbfa54bfd4b
eigen98 <terrantrace@gmail.com>
2023-10-11 17:40:45 +0900

    feat: '아직 내 지갑이 없어요' 팝업 뷰 컨트롤러 구현

--- DIDA/DIDA/Presentation/Popup/NoWalletAlertViewController.swift
+++ DIDA/DIDA/Presentation/Popup/NoWalletAlertViewController.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class NoWalletAlertViewController: UIViewController {
 
+    
+    @IBOutlet weak var goToSetPasswordButton: UIButton!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
 }
