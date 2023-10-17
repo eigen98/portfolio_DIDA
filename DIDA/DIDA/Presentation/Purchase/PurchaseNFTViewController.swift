464b705dbca802b95f7832f67d1bbf2456afb5a2
eigen98 <terrantrace@gmail.com>
2023-10-17 16:18:21 +0900

    feat: NFT 구매하기 뷰 컨트롤러, Xib 생성

---
+++ DIDA/DIDA/Presentation/Purchase/PurchaseNFTViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PurchaseNFTViewController.swift
+//  DIDA
+//
+//  Created by JeongMin Ko on 2023/10/17.
+//
+
+import UIKit
+
+class PurchaseNFTViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
