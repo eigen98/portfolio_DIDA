6e5e93a4674cb59f6b4bf3778dd3698ec1482984
eigen98 <terrantrace@gmail.com>
2023-10-17 16:30:30 +0900

    feat: NFT 구매 화면 뷰모델 생성

---
+++ DIDA/DIDA/Domain/Usecase/PurchaseNFT/PurchaseNFTViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PurchaseNFTViewModel.swift
+//  DIDA
+//
+//  Created by JeongMin Ko on 2023/10/17.
+//
+
+import Foundation
---
+++ DIDA/DIDA/Presentation/Purchase/View/PurchaseNFTView.swift
@@ -0,0 +1,8 @@
+//
+//  PurchaseNFTView.swift
+//  DIDA
+//
+//  Created by JeongMin Ko on 2023/10/17.
+//
+
+import Foundation
