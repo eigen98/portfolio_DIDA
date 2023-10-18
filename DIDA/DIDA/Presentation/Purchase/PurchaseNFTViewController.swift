896c58a8ff7dc18dedd94a389a25b776be45b3bb
eigen98 <terrantrace@gmail.com>
2023-10-18 23:05:09 +0900

    feat: NFT 구매화면 뷰 컨트롤러 구현

--- DIDA/DIDA/Presentation/Purchase/PurchaseNFTViewController.swift
+++ DIDA/DIDA/Presentation/Purchase/PurchaseNFTViewController.swift
@@ -4,26 +4,61 @@
 //
 //  Created by JeongMin Ko on 2023/10/17.
 //
+import RxSwift
 
-import UIKit
-
-class PurchaseNFTViewController: UIViewController {
-
+class NFTPurchaseViewController: BaseViewController {
+    private let scrollView = UIScrollView()
+    private let purchaseView = PurchaseNFTView()
+    private let viewModel = PurchaseNFTViewModel()
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+        bindViewModel()
+        bindEvent()
     }
+    
+    private func setupUI() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor,
+                                               constant: -70),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+        ])
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        purchaseView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(purchaseView)
+        
+        NSLayoutConstraint.activate([
+            purchaseView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            purchaseView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            purchaseView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            purchaseView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            purchaseView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        
+        setupBackButton()
     }
-    */
+    
+    override func bindViewModel() {
+        purchaseView.bind(to: viewModel)
 
+        viewModel.output.showPurchaseSuccess.subscribe(onNext: { [weak self] in
+        
+        }).disposed(by: disposeBag)
+        
+        viewModel.output.showPurchaseCancel.subscribe(onNext: { [weak self] in
+           
+        }).disposed(by: disposeBag)
+    }
+    
+    override func bindEvent() {
+       
+    }
 }
