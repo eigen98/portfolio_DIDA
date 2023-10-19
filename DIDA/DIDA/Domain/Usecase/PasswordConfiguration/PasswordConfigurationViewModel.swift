475b190251066554d41a2f600ea002162e9b3f3f
eigen98 <terrantrace@gmail.com>
2023-10-19 17:41:48 +0900

    feat: 뷰모델에 공개키 및 지갑 발급 로직 추가

--- DIDA/DIDA/Domain/Usecase/PasswordConfiguration/PasswordConfigurationViewModel.swift
+++ DIDA/DIDA/Domain/Usecase/PasswordConfiguration/PasswordConfigurationViewModel.swift
@@ -9,85 +9,171 @@ import Foundation
 import RxSwift
 import RxCocoa
 
+enum PasswordStep {
+    case setPassword
+    case confirmSetPassword
+    case enterPassword
+}
+
 class PasswordConfigurationViewModel: BaseViewModel {
-    enum PasswordStep {
-        case setPassword
-        case confirmSetPassword
-        case enterPassword
+    
+    struct Input {
+        let numberButtonTapped = PublishSubject<String>()
+        let deleteButtonTapped = PublishSubject<Void>()
     }
     
-    var passwordStep = BehaviorRelay<PasswordStep>(value: .setPassword)
-    private var setPassword = BehaviorRelay<String>(value: "")
-    private var confirmSetPassword = BehaviorRelay<String>(value: "")
-    private var enteredPassword = BehaviorRelay<String>(value: "")
-
-    var isPasswordMatched: Observable<Bool> {
-        return Observable.combineLatest(setPassword, enteredPassword)
-            .map { $0 == $1 }
+    struct Output {
+        let showError: Observable<String>
+        let passwordStep: BehaviorRelay<PasswordStep>
+        let encryptedPassword: BehaviorRelay<String?>
+        let walletIssuedSuccessfully: PublishSubject<Void>
     }
+    
+    let input: Input = Input()
+    let output: Output
+    
+    private let disposeBag = DisposeBag()
+    private let encryptionService: EncryptionService = RSAEncryptionService()
+    private let userRepository: UserRepository = UserRepositoryImpl()
 
+    private var password: String = ""
+    private var confirmPassword: String = ""
+    private var publicKey: String?
+    
+    private let showErrorSubject = PublishSubject<String>()
+    private let passwordStepRelay = BehaviorRelay<PasswordStep>(value: .setPassword)
+    private let encryptedPasswordRelay = BehaviorRelay<String?>(value: nil)
+    private let walletIssuedSuccessfullySubject = PublishSubject<Void>()
+
+    override init() {
+        self.output = Output(
+            showError: showErrorSubject.asObservable(),
+            passwordStep: passwordStepRelay,
+            encryptedPassword: encryptedPasswordRelay,
+            walletIssuedSuccessfully: walletIssuedSuccessfullySubject
+        )
+        super.init()
+        
+    }
+    
     override func bind() {
-
+        bindNumberButtonTapped()
+        bindDeleteButtonTapped()
     }
-
-    func numberButtonTapped(value: Int) {
-        switch passwordStep.value {
+    
+    private func bindNumberButtonTapped() {
+        input.numberButtonTapped
+            .subscribe(onNext: { [weak self] number in
+                guard let self = self else { return }
+                self.handleNumberInput(number)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func bindDeleteButtonTapped() {
+        input.deleteButtonTapped
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+                self.handleDeleteInput()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func handleNumberInput(_ number: String) {
+        switch passwordStepRelay.value {
         case .setPassword:
-            if setPassword.value.count < 6 {
-                setPassword.accept(setPassword.value + "\(value)")
-            } else {
-                self.showError.onNext(CustomError.passwordLimitExceeded)
+            password.append(number)
+            if password.count == 6 {
+                passwordStepRelay.accept(.confirmSetPassword)
             }
         case .confirmSetPassword:
-            if confirmSetPassword.value.count < 6 {
-                confirmSetPassword.accept(confirmSetPassword.value + "\(value)")
-            } else {
-                self.showError.onNext(CustomError.passwordLimitExceeded)
+            confirmPassword.append(number)
+            if confirmPassword.count == 6 {
+                verifyPasswordsAndEncrypt()
             }
         case .enterPassword:
-            if enteredPassword.value.count < 6 {
-                enteredPassword.accept(enteredPassword.value + "\(value)")
-            } else {
-                self.showError.onNext(CustomError.passwordLimitExceeded)
-            }
+            break
         }
     }
-
-    func deleteButtonTapped() {
-        switch passwordStep.value {
+    
+    private func handleDeleteInput() {
+        switch passwordStepRelay.value {
         case .setPassword:
-            if !setPassword.value.isEmpty {
-                setPassword.accept(String(setPassword.value.dropLast()))
+            if !password.isEmpty {
+                password.removeLast()
             }
         case .confirmSetPassword:
-            if !confirmSetPassword.value.isEmpty {
-                confirmSetPassword.accept(String(confirmSetPassword.value.dropLast()))
+            if !confirmPassword.isEmpty {
+                confirmPassword.removeLast()
             }
         case .enterPassword:
-            if !enteredPassword.value.isEmpty {
-                enteredPassword.accept(String(enteredPassword.value.dropLast()))
-            }
+            break
         }
     }
-
-    func moveToNextStep() {
-        switch passwordStep.value {
-        case .setPassword:
-            passwordStep.accept(.confirmSetPassword)
+    
+    private func verifyPasswordsAndEncrypt() {
+        if password == confirmPassword {
+            handlePasswordMatch()
+        } else {
+            showErrorSubject.onNext("Passwords do not match.")
+            resetPasswords()
+        }
+    }
+    
+    private func handlePasswordMatch() {
+        fetchPublicKey { [weak self] (publicKey, error) in
+            guard let self = self else { return }
             
-        case .confirmSetPassword:
-            if setPassword.value == confirmSetPassword.value {
-                passwordStep.accept(.enterPassword)
-            } else {
-                self.showError.onNext(CustomError.passwordMismatch)
+            if let publicKey = publicKey {
+                self.encryptPasswordAndIssueWallet(with: publicKey)
+            } else if let error = error {
+                self.handlePublicKeyFetchError(error)
             }
-        case .enterPassword:
-            if setPassword.value == enteredPassword.value {
+            
+            self.resetPasswords()
+        }
+    }
 
-            } else {
-                self.showError.onNext(CustomError.passwordMismatch)
+    private func encryptPasswordAndIssueWallet(with publicKey: String) {
+        if let encryptedPwd = encryptionService.encryptWithPublicKey(message: password, publicKeyString: publicKey) {
+            encryptedPasswordRelay.accept(encryptedPwd)
+            issueWallet(with: encryptedPwd)
+        } else {
+            showErrorSubject.onNext("Encryption failed.")
+        }
+    }
+
+    private func handlePublicKeyFetchError(_ error: Error) {
+        showErrorSubject.onNext("Failed to fetch public key.")
+    }
+
+    
+    private func issueWallet(with encryptedPwd: String) {
+        userRepository.issueWallet(payPwd: encryptedPwd,
+                                   checkPwd: encryptedPwd) { [weak self] (success, error) in
+            if let success = success, success {
+                self?.walletIssuedSuccessfullySubject.onNext(())
+            } else if let error = error {
+                self?.showErrorSubject.onNext(error.localizedDescription)
             }
         }
+    }
+    
+    private func fetchPublicKey(completion: @escaping (String?, Error?) -> ()) {
+        userRepository.fetchPublicKey { [weak self] (publicKey, error) in
+            if let publicKey = publicKey {
+                self?.publicKey = publicKey
+                completion(publicKey, nil)
+            } else if let error = error {
+                completion(nil, error)
+            }
+        }
+    }
+    
+    private func resetPasswords() {
+        password = ""
+        confirmPassword = ""
+        passwordStepRelay.accept(.setPassword)
     }
 }
 
@@ -95,4 +181,3 @@ enum CustomError: Error {
     case passwordLimitExceeded
     case passwordMismatch
 }
-
