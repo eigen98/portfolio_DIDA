72db1e082332b543aa4ce87e0a7493c587274b98
eigen98 <terrantrace@gmail.com>
2023-10-22 19:36:09 +0900

    feat: 네비게이션바 지갑 버튼 추가

--- DIDA/DIDA/Presentation/MyPage/MyPageViewController.swift
+++ DIDA/DIDA/Presentation/MyPage/MyPageViewController.swift
@@ -9,4 +9,18 @@ import UIKit
 
 class MyPageViewController: BaseViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTitleBar()
+    }
+    private func setTitleBar() {
+        let walletButton = UIButton()
+        walletButton.setImage(UIImage(named: "wallet-line"), for: .normal)
+        walletButton.addTarget(self, action: #selector(walletButtonTapped), for: .touchUpInside)
+        customTitleBar.rightItems = [walletButton]
+    }
+    
+    @objc func walletButtonTapped() {
+        
+    }
 }
