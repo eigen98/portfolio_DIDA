f2b728b604793ea45f5cb366bae8fdb03e59b8fb
eigen98 <terrantrace@gmail.com>
2023-11-21 15:32:27 +0900

    refactor: PasswordConfigurationViewController 호출 시 context 매개변수 추가

--- DIDA/DIDA/Presentation/PasswordConfiguration/PasswordConfigurationViewController.swift
+++ DIDA/DIDA/Presentation/PasswordConfiguration/PasswordConfigurationViewController.swift
@@ -14,14 +14,14 @@ class PasswordConfigurationViewController: BaseViewController {
     private let passwordConfigurationView = PasswordConfigurationView()
     private let disposeBag = DisposeBag()
     
-    init(with step: PasswordStep, nftId: Int? = nil) {
+    init(with step: PasswordStep, context: PasswordConfigurationContext, nftId: Int? = nil) {
         super.init(nibName: nil, bundle: nil)
-        self.viewModel = PasswordConfigurationViewModel(initialStep: step, nftId: nftId)
+        self.viewModel = PasswordConfigurationViewModel(initialStep: step, context: context, nftId: nftId)
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.viewModel = PasswordConfigurationViewModel(initialStep: .setPassword)
+        self.viewModel = PasswordConfigurationViewModel(initialStep: .setPassword, context: .initialSetup)
     }
     
     override func viewDidLoad() {
@@ -80,55 +80,100 @@ class PasswordConfigurationViewController: BaseViewController {
         
         viewModel.output.passwordCheckState
             .bind(onNext: { [weak self] state in
-                switch state {
-                case .initial:
-                    self?.passwordConfigurationView.hideWrongCountLabel()
-                case .matched:
-                    self?.passwordConfigurationView.hideWrongCountLabel()
-                    
-                    guard let presentingVC = self?.presentingViewController else { return }
-                    guard let nftId = self?.viewModel.nftId else { return }
-                    let purchaseVC = NFTPurchaseViewController(currentNFTID: nftId)
-                    self?.dismiss(animated: true, completion: { [weak presentingVC] in
-                        if let navController = presentingVC as? UINavigationController {
-                            navController.pushViewController(purchaseVC, animated: false)
-                        }
-                    })
-                    
-                case .mismatched(let count):
-                    self?.passwordConfigurationView.showWrongCountLabel(with: count)
-                    self?.passwordConfigurationView.setTitle("비밀번호가 일치하지 않아요")
-                    self?.passwordConfigurationView.setSubtitle("다시 눌러주세요")
-                    
-                case .exceededLimit:
-                    self?.passwordConfigurationView.hideWrongCountLabel()
-                    let alertVC = PasswordChangeAlertViewController()
-                    alertVC.modalPresentationStyle = .overFullScreen
-                    self?.present(alertVC, animated: false)
-                case .passwordChanged:
-                    guard let presentingVC = self?.presentingViewController else { return }
-                    self?.dismiss(animated: false,
-                                  completion: { [weak presentingVC] in
-                        presentingVC?.dismiss(animated: false)
-                    })
-                case .error(let message):
-                    break
-                default:
-                    break
-                }
+                self?.handlePasswordCheckState(state)
             })
             .disposed(by: disposeBag)
     }
     
-    private func setupUI() {
+    private func handlePasswordCheckState(_ state: PasswordCheckState) {
+        switch state {
+        case .initial:
+            passwordConfigurationView.hideWrongCountLabel()
+            
+        case .matched:
+            passwordConfigurationView.hideWrongCountLabel()
+            handleMatchedPasswordState()
+            
+        case .mismatched(let count):
+            handleMismatchedPasswordState(count)
+            
+        case .exceededLimit:
+            handleExceededLimitState()
+            
+        case .passwordChanged:
+            handlePasswordChangedState()
+            
+        case .error(let message):
+            handleErrorState(message)
+        }
+    }
+    
+    private func handleMatchedPasswordState() {
+        switch viewModel.context {
+        case .nftPurchase(let nftId):
+            navigateToNFTPurchaseViewController(nftId: nftId)
+        case .coinSwap:
+            performCoinSwap()
+        default:
+            break
+        }
+    }
+    
+    private func navigateToNFTPurchaseViewController(nftId: Int) {
+        
+        self.passwordConfigurationView.hideWrongCountLabel()
+        guard let presentingVC = self.presentingViewController else { return }
+        guard let nftId = self.viewModel.nftId else { return }
+        let purchaseVC = NFTPurchaseViewController(currentNFTID: nftId)
+        self.dismiss(animated: true, completion: { [weak presentingVC] in
+            if let navController = presentingVC as? UINavigationController {
+                navController.pushViewController(purchaseVC, animated: false)
+            }
+        })
+    }
 
+    private func performCoinSwap() {
+
+    }
+
+    private func handleMismatchedPasswordState(_ count: Int) {
+        passwordConfigurationView.showWrongCountLabel(with: count)
+        passwordConfigurationView.setTitle("비밀번호가 일치하지 않아요")
+        passwordConfigurationView.setSubtitle("다시 눌러주세요")
+    }
+
+    private func handleExceededLimitState() {
+        self.passwordConfigurationView.hideWrongCountLabel()
+        let alertVC = PasswordChangeAlertViewController()
+        alertVC.modalPresentationStyle = .overFullScreen
+        self.present(alertVC, animated: false)
+    }
+
+    private func handlePasswordChangedState() {
+        guard let presentingVC = self.presentingViewController else { return }
+        self.dismiss(animated: false,
+                      completion: { [weak presentingVC] in
+            presentingVC?.dismiss(animated: false)
+        })
+    }
+
+    private func handleErrorState(_ message: String) {
+        
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    private func setupUI() {
+        
         passwordConfigurationView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(passwordConfigurationView)
         setupConstraints()
     }
     
     private func setupConstraints() {
-       
+        
         passwordConfigurationView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
         passwordConfigurationView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         passwordConfigurationView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
