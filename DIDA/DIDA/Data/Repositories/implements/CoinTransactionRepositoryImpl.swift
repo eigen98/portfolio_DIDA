a7238e791c61515ba8ad6fa0baccef8fa0f0dd9d
eigen98 <terrantrace@gmail.com>
2023-11-22 17:14:37 +0900

    feat: CoinTransactionRepository 구현체 추가 및 스왑 API 요청 로직 구현

--- DIDA/DIDA/Data/Repositories/implements/CoinTransactionRepositoryImpl.swift
+++ DIDA/DIDA/Data/Repositories/implements/CoinTransactionRepositoryImpl.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import RxSwift
+
+class CoinTransactionRepositoryImpl: CoinTransactionRepository {
+    
+    private let disposeBag = DisposeBag()
+
+    func swapKlayToDida(payPwd: String, coin: Int, completion: @escaping (Result<Bool, Error>) -> Void) {
+        APIClient.request(.swapKlayToDida(payPwd: payPwd, coin: coin))
+            .map { response -> Bool in
+                if response.statusCode == 200 {
+                    return true
+                } else {
+                    let error = try response.map(BaseErrorResponseDTO.self)
+                    throw DidaError.apiError(error)
+                }
+            }
+            .subscribe(onSuccess: { success in
+                completion(.success(success))
+            }, onFailure: { error in
+                completion(.failure(error))
+            }).disposed(by: disposeBag)
+    }
+
+    func swapDidaToKlay(payPwd: String, coin: Int, completion: @escaping (Result<Bool, Error>) -> Void) {
+        APIClient.request(.swapDidaToKlay(payPwd: payPwd, coin: coin))
+            .map { response -> Bool in
+                if response.statusCode == 200 {
+                    return true
+                } else {
+                    let error = try response.map(BaseErrorResponseDTO.self)
+                    throw DidaError.apiError(error)
+                }
+            }
+            .subscribe(onSuccess: { success in
+                completion(.success(success))
+            }, onFailure: { error in
+                completion(.failure(error))
+            }).disposed(by: disposeBag)
+    }
+}
